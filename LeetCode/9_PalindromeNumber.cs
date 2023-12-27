4511e6c63c795c864e7d1e29e440328c70fe1ad6
Skowoo <skowoo@tlen.pl>
2023-12-27 09:57:49 +0100

    Add project files.

---
+++ LeetCode/9_PalindromeNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode
+{
+    internal class _9_PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            string input = x.ToString();
+            for (int i = 0; i < input.Length / 2; i++)
+                if (input[i] != input[input.Length - 1 - i])
+                    return false;
+
+            return true;
+        }
+    }
+}

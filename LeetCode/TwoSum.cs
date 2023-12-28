b91aa9bfc8f3c1a2832aa021acc695ca204c9a5c
Skowoo <skowoo@tlen.pl>
2023-12-28 12:06:54 +0100

    Two Sum done

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode
+{
+    internal class TwoSum
+    {
+        public static int[] TwoSumFunc(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 0; j < nums.Length; j++)
+                    if (nums[i] + nums[j] == target && i != j)
+                        return new int[] { i, j };
+
+            return new int[] { 0, 0 };
+        }
+    }
+}

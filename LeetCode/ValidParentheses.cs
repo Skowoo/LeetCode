b1ebf531c2791df07541f5f69325b8481eaa6b4f
Skowoo <skowoo@tlen.pl>
2023-12-28 19:41:59 +0100

    Valid Parentheses done

---
+++ LeetCode/ValidParentheses.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode
+{
+    internal class ValidParentheses
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new();
+
+            foreach (char c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push('(');
+                        break;
+                    case ')':
+                        if (stack.Count > 0)
+                            if (stack.Peek() == '(')
+                            {
+                                stack.Pop();
+                                break;
+                            }
+
+                        return false;
+                    case '[':
+                        stack.Push('[');
+                        break;
+                    case ']':
+                        if (stack.Count > 0)
+                            if (stack.Peek() == '[')
+                            {
+                                stack.Pop();
+                                break;
+                            }
+
+                        return false;
+                    case '{':
+                        stack.Push('{');
+                        break;
+                    case '}':
+                        if (stack.Count > 0)
+                            if (stack.Peek() == '{')
+                            {
+                                stack.Pop();
+                                break;
+                            }
+
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}

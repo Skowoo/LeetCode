06762f1c9cba875fb835b1d023fa583ac21dac88
Skowoo <skowoo@tlen.pl>
2023-12-29 13:54:42 +0100

    Remove Element Done

---
+++ LeetCode/RemoveElement.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    internal class RemoveElement
+    {
+        public static int RemoveElementFunc(int[] nums, int val)
+        {
+            int noValCounter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] != val)
+                    nums[noValCounter++] = nums[i];
+
+            return noValCounter;
+        }
+    }
+}

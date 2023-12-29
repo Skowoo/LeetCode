4788155636f8d45cdf7bd9ada15a7cb71dc83318
Skowoo <skowoo@tlen.pl>
2023-12-29 13:01:24 +0100

    Remove Duplicates From Sorted Array done

---
+++ LeetCode/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    internal class RemoveDuplicatesFromSortedArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2)
+                return nums.Length;
+
+            int distinctNumberCounter = 1, lastNumber = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != lastNumber)
+                {
+                    lastNumber = nums[i];
+                    nums[distinctNumberCounter] = nums[i];
+                    distinctNumberCounter++;
+                }
+            }
+
+            return distinctNumberCounter;
+        }
+    }
+}

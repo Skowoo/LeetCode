b61ff8cf665cae70f955bde0c3a2ed425766bbcf
Skowoo <skowoo@tlen.pl>
2023-12-30 14:19:36 +0100

    String To Integer (ATOI) done

---
+++ LeetCode/StringToInt_ATOI.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode
+{
+    internal class StringToInt_ATOI
+    {
+        public static int MyAtoi(string s)
+        {
+            s = s.Trim();
+
+            if (s.Length < 1)
+                return 0;
+
+            bool isNegative = s.StartsWith('-');
+            bool isSigned = s.StartsWith('+');
+
+            for (int i = isNegative || isSigned ? 1 : 0; i < s.Length; i++)
+                if (!char.IsDigit(s[i]))
+                    s = s.Remove(i);
+
+            Console.WriteLine(s);
+
+            if (s.Length < 1)
+                return 0;
+
+            bool parsedSuccess = int.TryParse(s, out int result);
+
+            if (parsedSuccess)
+                return result;
+            else if (s.Length < 8)
+                return 0;
+            else if (isNegative)
+                return int.MinValue;
+            else
+                return int.MaxValue;
+        }
+    }
+}

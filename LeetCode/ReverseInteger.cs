6408b7e33af89fc74c0939bfc9ac375fcd2debdf
Skowoo <skowoo@tlen.pl>
2023-12-30 12:44:43 +0100

    Reverse Integer done

---
+++ LeetCode/ReverseInteger.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode
+{
+    internal class ReverseInteger
+    {
+        public static int Reverse(int x)
+        {
+            bool negative = x < 0;
+            string inputNumber = x.ToString().Trim('-');
+            string outputNumber = negative ? "-" : "";
+            for (int i = 1; i <= inputNumber.Length; i++)
+                outputNumber += inputNumber[^i];
+
+            bool parsed = int.TryParse(outputNumber, out int parseResult);
+            return parsed ? parseResult : 0;
+        }
+    }
+}

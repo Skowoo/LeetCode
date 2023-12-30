9cef2459c75c497c58add6ae64f6912085a15f19
Skowoo <skowoo@tlen.pl>
2023-12-30 08:26:41 +0100

    FindIndexOfFirstOccurenceInAString done

---
+++ LeetCode/FindIndexOfFirstOccurenceInAString.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode
+{
+    internal class FindIndexOfFirstOccurenceInAString
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            Regex regex = new Regex(needle);
+            MatchCollection matches = regex.Matches(haystack);
+            Match? result = matches.FirstOrDefault();
+            if (result != null)
+                return result.Index;
+            else return -1;
+        }
+    }
+}

c57c2b122bb3fca7fcee3818d83aa3ee80fb58c1
Skowoo <skowoo@tlen.pl>
2023-12-31 09:45:59 +0100

    Median of two sorted arrays done

---
+++ LeetCode/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode
+{
+    internal class MedianOfTwoSortedArrays
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var combined = nums1.Concat(nums2).ToArray();
+            Array.Sort(combined);
+
+            int centerIndex = combined.Length / 2;
+
+            if (combined.Length % 2 == 1)
+                return combined[centerIndex];
+            else
+                return (double)(combined[centerIndex - 1] + combined[centerIndex]) / 2;
+        }
+    }
+}

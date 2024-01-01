24cdab8b360fdb1eb52baefd4ea33eba9d8d136f
Skowoo <skowoo@tlen.pl>
2024-01-01 10:42:34 +0100

    Assign Cookies done

---
+++ LeetCode/Easy/AssignCookies.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal class AssignCookies
+    {
+        public static int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+            int ChildrenCounter = 0, Cookie = 0;
+
+            while (Cookie < s.Length && ChildrenCounter < g.Length)
+            {
+                if (s[Cookie] >= g[ChildrenCounter])
+                {
+                    ChildrenCounter++;
+                }
+                Cookie++;
+            }
+            return ChildrenCounter;
+        }
+    }
+}

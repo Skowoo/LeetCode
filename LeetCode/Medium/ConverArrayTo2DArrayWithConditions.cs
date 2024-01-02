e56781a82cb6218d7b27cf9a803dfb000eaa7298
Skowoo <skowoo@tlen.pl>
2024-01-02 10:02:14 +0100

    Convert an Array Into a 2D Array With Conditions done

---
+++ LeetCode/Medium/ConverArrayTo2DArrayWithConditions.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ConverArrayTo2DArrayWithConditions
+    {
+        public static IList<IList<int>> FindMatrix(int[] nums)
+        {
+            List<List<int>> resultArray = new()
+            {
+                new List<int> { },
+            };
+
+            foreach (int num in nums)
+                for (int i = 0; i < resultArray.Count + 1; i++)
+                {
+                    if (i == resultArray.Count)
+                        resultArray.Add(new List<int>(num));
+
+                    if (!resultArray[i].Contains(num))
+                    {
+                        resultArray[i].Add(num);
+                        break;
+                    }
+                }
+
+            return resultArray.ToArray();
+        }
+    }
+}

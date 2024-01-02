2fbe90a17cfe644889cb4e49b7c779e5ae415a5e
Skowoo <skowoo@tlen.pl>
2024-01-02 21:52:33 +0100

    Length of last word done

---
+++ LeetCode/Easy/LengthOfLastWord.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Easy
+{
+    internal class LengthOfLastWord
+    {
+
+        public static int LengthOfLastWordSlower(string s)
+        {
+            int counter = 0;
+            bool initialized = false;
+            for (int i = 1; i < s.Length + 1; i++)
+                if (s[^i] != ' ' && initialized)
+                    counter++;
+                else if (initialized)
+                    return counter;
+                else if (s[^i] != ' ' && !initialized)
+                {
+                    initialized = true;
+                    counter++;
+                }
+
+            return counter;
+        }
+
+        public static int LengthOfLastWordMedium(string s)
+        {
+            s = s.Trim();
+            int counter = 0;
+            for (int i = 1; i < s.Length + 1; i++)
+                if (s[^i] != ' ')
+                    counter++;
+                else
+                    return counter;
+            return counter;
+        }
+
+        public static int LengthOfLastWordSlow(string s)
+        {
+            s = s.Trim();
+            string[] strings = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return strings[^1].Length;
+        }
+    }
+}

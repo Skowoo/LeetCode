a19ed69d4eebd16178062fc2713ee78c189a0fcd
Skowoo <skowoo@tlen.pl>
2024-01-03 08:36:26 +0100

    Number of laser beams in a bank done

---
+++ LeetCode/Medium/NumberOfLaserBeamsInABank.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Medium
+{
+    internal class NumberOfLaserBeamsInABank
+    {
+        public static int NumberOfBeams(string[] bank)
+        {
+            int beamsCounter = 0, previousNodes = 0;
+
+            for (int i = 0; i < bank.Length; i++)
+            {
+                int nodes = bank[i].ToCharArray().Where(x => x == '1').Count();
+                if (nodes != 0)
+                {
+                    beamsCounter += nodes * previousNodes;
+                    previousNodes = nodes;
+                }
+            }
+
+            return beamsCounter;
+        }
+    }
+}

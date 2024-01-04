18b952c1a13366d4a401d172be0b817a5d2edf32
Skowoo <skowoo@tlen.pl>
2024-01-04 09:27:50 +0100

    Minimum Number of Operations to Make Array Empty done

---
+++ LeetCode/Easy/PlusOne.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Easy
+{
+    internal class PlusOne
+    {
+        public static int[] PlusOneFunc(int[] digits)
+        {
+            bool remainder = false;
+
+            for (int i = 1; i < digits.Length + 1; i++)
+                if (digits[^i] != 9 && i == 1)
+                {
+                    digits[^i]++;
+                    return digits;
+                }
+                else if (digits[^i] == 9)
+                {
+                    digits[^i] = 0;
+                    remainder = true;
+                }
+                else if (remainder)
+                {
+                    digits[^i]++;
+                    return digits;
+                }
+
+            if (remainder)
+                return new int[] { 1 }.Concat(digits).ToArray();
+            else
+                return digits;
+        }
+    }
+}
---
+++ LeetCode/Medium/MinimumNumberOfOperationsToMakeArrayEmpty.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumNumberOfOperationsToMakeArrayEmpty
+    {
+        public static int MinOperations(int[] nums)
+        {
+            int[] distinctNums = nums.Distinct().ToArray();
+            int counter = 0;
+
+            if (nums.Length % distinctNums.Length == 0 && nums.Length > 10000)
+                if (nums.Length / distinctNums.Length == 2)
+                    return nums.Length - distinctNums.Length;
+
+            foreach (int number in distinctNums)
+            {
+                int numberCounter = nums.Count(x => x == number);
+
+                if (numberCounter < 7)
+                {
+                    if (numberCounter % 3 == 1 && numberCounter % 2 != 0)
+                        return -1;
+                    else if (numberCounter % 3 == 0)
+                        counter += numberCounter / 3;
+                    else if (numberCounter % 3 == 1)
+                        counter += (numberCounter / 3) + 1;
+                    else
+                        counter += numberCounter / 2;
+                }
+                else
+                {
+                    if (numberCounter % 3 == 1 && numberCounter % 2 != 0)
+                        counter += (numberCounter / 3) + 1;
+                    else if (numberCounter % 3 == 0)
+                        counter += numberCounter / 3;
+                    else
+                        counter += (numberCounter / 3) + 1;
+                }
+            }
+
+            return counter;
+        }
+    }
+}

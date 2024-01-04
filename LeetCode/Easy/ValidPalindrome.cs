1b476fd2330efcb8d0d1ebdf7c7e561e40d28be0
Skowoo <skowoo@tlen.pl>
2024-01-04 12:45:27 +0100

    Valid Palindrome done

---
+++ LeetCode/Easy/ValidPalindrome.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Easy
+{
+    internal class ValidPalindrome
+    {
+        public static bool IsPalindrome(string s)
+        {
+            Regex rgx = new("[^a-z]");
+            s = rgx.Replace(s.ToLower(), "");
+
+            Console.WriteLine(s);
+
+            for (int i = 0; i < s.Length / 2; i++)
+                if (s[i] != s[^(i + 1)])
+                    return false;
+
+            return true;
+        }
+    }
+}

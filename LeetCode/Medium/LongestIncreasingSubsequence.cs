d22b92e4b6ae3ca0af7aeb93f1de53c81efe6568
Skowoo <skowoo@tlen.pl>
2024-01-05 10:22:18 +0100

    Longest increasing subsequence Done

---
+++ LeetCode/Medium/LongestIncreasingSubsequence.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Medium
+{
+    internal class LongestIncreasingSubsequence
+    {
+        public static int LengthOfLIS(int[] nums)
+        {
+            int[] auxArray = Enumerable.Repeat(1, nums.Length).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 0; j < i; j++)
+                    if (nums[i] > nums[j])
+                        auxArray[i] = Math.Max(auxArray[i], 1 + auxArray[j]);
+
+            return auxArray.Max();
+        }
+    }
+}

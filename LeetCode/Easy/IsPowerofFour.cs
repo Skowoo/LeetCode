c5d9b18f5a465b0b79815b7ecc1d01ccba993343
Skowoo <skowoo@tlen.pl>
2024-01-06 19:16:24 +0100

    Is power of four done

---
+++ LeetCode/Easy/IsPowerofFour.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal class IsPowerofFour
+    {
+        static bool IsPowerOfFour(int n)
+        {
+            if (n == 4 || n == 1)
+                return true;
+            else if (n % 4 != 0 || n < 0)
+                return false;
+            else if (n > 4)
+                return IsPowerOfFour(n / 4);
+
+            return false;
+        }
+    }
+}

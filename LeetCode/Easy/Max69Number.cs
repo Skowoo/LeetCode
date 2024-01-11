96cf36c75fb965b29adaa821c2992cee489045d8
Skowoo <skowoo@tlen.pl>
2024-01-11 13:26:55 +0100

    Maximum 69 number done

---
+++ LeetCode/Easy/Max69Number.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal class Max69Number
+    {
+
+        public static int Maximum69Number(int num)
+        {
+            var numString = num.ToString().ToCharArray();
+
+            for (int i = 0; i < numString.Length; i++)
+                if (numString[i] == '6')
+                {
+                    numString[i] = '9';
+                    return int.Parse(numString);
+                }
+
+            return int.Parse(numString);
+        }
+    }
+}

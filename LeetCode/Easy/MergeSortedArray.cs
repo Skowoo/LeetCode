2f1b7378d1de0e5312de57d72579bd6b30de67d8
Skowoo <skowoo@tlen.pl>
2024-01-11 13:32:25 +0100

    Merge sorted array done

---
+++ LeetCode/Easy/MergeSortedArray.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MergeSortedArray
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m; i < m + n; i++)
+                nums1[i] = nums2[i - m];
+
+            Array.Sort(nums1);
+        }
+    }
+}

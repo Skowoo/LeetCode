8def05c99351ff601c57b9f5d738793e3bf91cf6
Skowoo <skowoo@tlen.pl>
2024-01-11 11:34:06 +0100

    Running sum of 1D array done.

---
+++ LeetCode/Easy/RunningSumOf1dArray.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal class RunningSumOf1dArray
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                nums[i] = sum;
+            }
+            return nums;
+        }
+    }
+}

a4f270eeaeede81a803635fa9879c6cd5c9d775d
Skowoo <skowoo@tlen.pl>
2024-01-12 09:38:31 +0100

    Determine If String Halves Are Alike done

---
+++ LeetCode/Easy/DetermineIfStringHalvesAreAlike.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    internal class DetermineIfStringHalvesAreAlike
+    {
+        public static bool HalvesAreAlike(string s)
+        {
+            int Counter = 0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            for (int i = 0; i < s.Length; i++)
+                if (vowels.Contains(s[i]))
+                    if (i < s.Length / 2)
+                        Counter++;
+                    else
+                        Counter--;
+
+            return Counter == 0;
+        }
+    }
+}

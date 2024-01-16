65728e87c3f79000b8c7da42b3a70062e8a604df
Skowoo <skowoo@tlen.pl>
2024-01-16 08:58:31 +0100

    InsertDeleteGetRandomWithDuplicates done

---
+++ LeetCode/Hard/InsertDeleteGetRandomWithDuplicates.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Hard
+{
+    internal class InsertDeleteGetRandomWithDuplicates
+    {
+        public static void TestMain()
+        {
+            var a = new RandomizedCollection();
+            a.Insert(1);
+            a.Insert(1);
+            a.Remove(1);
+            Console.WriteLine(a.GetRandom() == 1);
+        }
+
+        public class RandomizedCollection
+        {
+
+            List<int> _values;
+
+            public RandomizedCollection()
+            {
+                _values = new List<int>();
+            }
+
+            public bool Insert(int val)
+            {
+                bool exists = _values.Contains(val);
+                _values.Add(val);
+                return !exists;
+            }
+
+            public bool Remove(int val)
+            {
+                if (!_values.Contains(val))
+                    return false;
+
+                _values.Remove(val);
+                return true;
+            }
+
+            public int GetRandom()
+            {
+                Random rnd = new();
+                return _values.ElementAt(rnd.Next(0, _values.Count));
+            }
+        }
+    }
+}

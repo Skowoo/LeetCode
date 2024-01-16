1307c2d9ccfdcb509dce69ba1b3f797d901cc0a3
Skowoo <skowoo@tlen.pl>
2024-01-16 08:34:56 +0100

    InsertDeleteGetRandom done

---
+++ LeetCode/Medium/InsertDeleteGetRandom.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Medium
+{
+    internal class InsertDeleteGetRandom
+    {
+        public class RandomizedSet
+        {
+            List<int> _values;
+
+            public RandomizedSet()
+            {
+                _values = new List<int>();
+            }
+
+            public bool Insert(int val)
+            {
+                if (_values.Contains(val))
+                    return false;
+
+                _values.Add(val);
+                return true;
+            }
+
+            public bool Remove(int val)
+            {
+                if (!_values.Contains(val))
+                    return false;
+
+                _values.Remove(val);
+                return true;
+            }
+
+            public int GetRandom()
+            {
+                Random rnd = new();
+                return _values.ElementAt(rnd.Next(0, _values.Count));
+            }
+        }
+    }
+}

248b10f393e309c26e176f639cea1b32a973694e
Skowoo <skowoo@tlen.pl>
2024-01-17 08:32:42 +0100

    UniqueNumberOfOccurencies done

---
+++ LeetCode/Easy/UniqueNumberOfOccurencies.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    internal class UniqueNumberOfOccurencies
+    {
+
+        public static bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> occurencies = new();
+
+            foreach (int i in arr)
+                if (occurencies.ContainsKey(i))
+                    occurencies[i]++;
+                else
+                    occurencies.Add(i, 1);
+
+            return occurencies.Values.Count == occurencies.Values.Distinct().Count();
+        }
+    }
+}

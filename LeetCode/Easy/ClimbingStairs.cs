6cf3004f54846798ccffa5329df8c384e98cc214
Skowoo <skowoo@tlen.pl>
2024-01-18 08:34:44 +0100

    ClimbingStairs done

---
+++ LeetCode/Easy/ClimbingStairs.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ClimbingStairs
+    {
+        public static int ClimbStairs(int n)
+        {
+            if (n == 1)
+                return 1;
+            else if (n == 2)
+                return 2;
+
+            int[] steps = new int[n + 1];
+            steps[0] = 1;
+            steps[1] = 2;
+
+            for (int i = 2; i <= n; i++)
+                steps[i] = steps[i - 2] + steps[i - 1];
+
+            return steps[n - 1];
+        }
+    }
+}

8db37197b01ad470cf7de1b1ac02c6d60244739d
Skowoo <skowoo@tlen.pl>
2024-01-23 19:13:04 +0100

    ContainsDuplicate done

---
+++ LeetCode/Easy/ContainsDuplicate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class ContainsDuplicate
+    {
+        public static bool ContainsDuplicateFunc(int[] nums) => nums.Length != nums.Distinct().Count();
+    }
+}

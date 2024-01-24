079d724f7543d2b14660b62d6ad88681205e2576
Skowoo <skowoo@tlen.pl>
2024-01-24 21:35:27 +0100

    PascalTriangle done

---
+++ LeetCode/Easy/PascalTriangle.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Easy
+{
+    internal class PascalTriangle
+    {
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            int[][] result = new int[numRows][];
+
+            for (int i = 1; i <= numRows; i++)
+            {
+                int[] currentArray = new int[i];
+                currentArray[0] = 1;
+
+                for (int j = 1; j < i; j++)
+                {
+                    if (j == i - 1)
+                    {
+                        currentArray[j] = 1;
+                        break;
+                    }
+
+                    currentArray[j] = result[i - 2][j - 1] + result[i - 2][j];
+                }
+
+                result[i - 1] = currentArray;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
+
+//var array1 = Generate(5);
+
+//foreach (var x in array1)
+//{
+//    foreach (var y in x)
+//        Console.Write(y);
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine(array1.Count);

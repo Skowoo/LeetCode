3cb3a46aae0cf929bcf651a13a36fefaabac9697
Skowoo <skowoo@tlen.pl>
2024-01-26 14:12:26 +0100

    ANumberAfterADoubleReversal done

---
+++ LeetCode/Easy/ANumberAfterADoubleReversal.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ANumberAfterADoubleReversal
+    {
+        public static bool IsSameAfterReversals(int num)
+        {
+            if (num < 10)
+                return true;
+
+            if (num % 10 != 0)
+                return true;
+
+            return false;
+        }
+    }
+}

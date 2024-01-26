5900c9c4470c8cdc4dbfda3afb52d9a922d56a41
Skowoo <skowoo@tlen.pl>
2024-01-26 13:25:31 +0100

    ReverseWordsInAString3 done

---
+++ LeetCode/Easy/ReverseWordsInAString3.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ReverseWordsInAString3
+    {
+        public static string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ').ToArray();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var tempArray = words[i].ToCharArray();
+                Array.Reverse(tempArray);
+                words[i] = new string(tempArray);
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}

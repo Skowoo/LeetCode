9adb8f465053d348cc16beb5f695cbc6e092bfed
Skowoo <skowoo@tlen.pl>
2024-01-27 12:28:57 +0100

    FindTheDifference done

---
+++ LeetCode/Easy/FindTheDifference.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    internal class FindTheDifference
+    {
+        public static char FindTheDifferenceFunc(string s, string t)
+        {
+            Dictionary<char, int> sDictionary = new();
+            Dictionary<char, int> tDictionary = new();
+
+            foreach (char c in s)
+                if (sDictionary.ContainsKey(c))
+                    sDictionary[c]++;
+                else
+                    sDictionary.Add(c, 1);
+
+            foreach (char c in t)
+                if (tDictionary.ContainsKey(c))
+                    tDictionary[c]++;
+                else
+                    tDictionary.Add(c, 1);
+
+            foreach (char c in sDictionary.Keys)
+                tDictionary[c] -= sDictionary[c];
+
+            return tDictionary.Where(x => x.Value == 1).Single().Key;
+        }
+    }
+}

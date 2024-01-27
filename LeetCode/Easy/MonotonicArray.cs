15b7767ad96bf6ef15ab92cdc2578c4f1cecc57a
Skowoo <skowoo@tlen.pl>
2024-01-27 11:56:09 +0100

    MonotonicArray done

---
+++ LeetCode/Easy/MonotonicArray.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MonotonicArray
+    {
+        public static bool IsMonotonic(int[] nums)
+        {
+            bool result = true;
+
+            if (nums.Length < 2)
+                return result;
+
+            bool rising = false;
+            bool decreasing = false;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (!decreasing && !rising)
+                {
+                    if (nums[i] < nums[i - 1])
+                        decreasing = true;
+                    if (nums[i] > nums[i - 1])
+                        rising = true;
+                }
+                else
+                {
+                    if (rising)
+                    {
+                        if (nums[i] < nums[i - 1])
+                            return false;
+                    }
+                    else if (nums[i] > nums[i - 1])
+                        return false;
+                }
+            }
+
+            return result;
+        }
+    }
+}

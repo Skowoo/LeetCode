6cfb932894aa6b42cec2f1c3573125c1b77f9434
Skowoo <skowoo@tlen.pl>
2024-01-28 19:39:16 +0100

    ElementAppearingMoreThan25_InSortedArray done

---
+++ LeetCode/Easy/ElementAppearingMoreThan25%InSortedArray.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ElementAppearingMoreThan25_InSortedArray
+    {
+        public static int FindSpecialInteger(int[] arr)
+        {
+            Dictionary<int, int> occurences = new();
+
+            foreach (int i in arr)
+                if (occurences.ContainsKey(i))
+                {
+                    occurences[i]++;
+                    if (occurences[i] > arr.Length / 4 + 1)
+                        return i;
+                }
+                else
+                    occurences.Add(i, 1);
+
+            return occurences.Where(x => x.Value == occurences.Values.Max()).Single().Key;
+        }
+    }
+}

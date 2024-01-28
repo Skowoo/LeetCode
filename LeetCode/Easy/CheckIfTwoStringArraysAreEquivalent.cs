21c9342a694bf21035c4a7d352c36df895742489
Skowoo <skowoo@tlen.pl>
2024-01-28 08:20:01 +0100

    CheckIfTwoStringArraysAreEquivalent done

---
+++ LeetCode/Easy/CheckIfTwoStringArraysAreEquivalent.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCode.Easy
+{
+    internal class CheckIfTwoStringArraysAreEquivalent
+    {
+        public static bool ArrayStringsAreEqual(string[] word1, string[] word2) => string.Join("", word1) == string.Join("", word2);
+    }
+}

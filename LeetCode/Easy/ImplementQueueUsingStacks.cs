e09049ef0d2fcfd71c1ce52aeb3ab4b285b54efb
Skowoo <skowoo@tlen.pl>
2024-01-29 09:11:33 +0100

    ImplementQueueUsingStacks done

---
+++ LeetCode/Easy/ImplementQueueUsingStacks.cs
@@ -0,0 +1,54 @@
+﻿#pragma warning disable IDE0044 // Add readonly modifier
+
+namespace LeetCode.Easy
+{
+    public class MyQueue
+    {
+        private Stack<int> main;
+        private Stack<int> temp;
+
+        public MyQueue()
+        {
+            main = new Stack<int>();
+            temp = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            if (main.Count == 0)
+                main.Push(x);
+            else
+            {
+                while (!(main.Count == 0))
+                    temp.Push(main.Pop());
+
+                main.Push(x);
+
+                while (!(temp.Count == 0))
+                    main.Push(temp.Pop());
+            }
+        }
+
+        public int Pop() => main.Pop();
+
+        public int Peek() => main.Peek();
+
+        public bool Empty() => main.Count == 0;
+    }
+
+    //static void Main()
+    //{
+    //    var test = new MyQueue();
+    //    test.Push(1);
+    //    test.Push(2);
+    //    test.Push(3);
+    //    test.Push(4);
+    //    test.Push(5);
+    //    Console.WriteLine(test.Peek());
+    //    Console.WriteLine(test.Pop());
+    //    Console.WriteLine(test.Pop());
+    //    Console.WriteLine(test.Pop());
+    //    Console.WriteLine(test.Pop());
+    //    Console.WriteLine(test.Pop());
+    //}
+}

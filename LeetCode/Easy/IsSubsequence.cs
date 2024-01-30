af82578b8da02a678a01f87c5329deeb705af625
Skowoo <skowoo@tlen.pl>
2024-01-30 09:34:12 +0100

    IsSubsequence done

---
+++ LeetCode/Easy/IsSubsequence.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy
+{
+    internal class IsSubsequence
+    {
+        public static bool IsSubsequenceFunc(string s, string t)
+        {
+            if (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t))
+                return true;
+
+            if (string.IsNullOrEmpty(s) && !string.IsNullOrEmpty(t))
+                return true;
+
+            if (!string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t))
+                return false;
+
+            int result = 0;
+
+            foreach (char c in t)
+                if (s[result] == c)
+                {
+                    result++;
+                    if (result == s.Length)
+                        return true;
+                }
+
+
+            return result == s.Length;
+        }
+    }
+}

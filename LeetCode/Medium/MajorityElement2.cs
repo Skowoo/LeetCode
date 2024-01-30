3ca0d24795dc6a4c0dee2d2a0009f59019eb4013
Skowoo <skowoo@tlen.pl>
2024-01-30 12:30:39 +0100

    MajorityElement2 done

---
+++ LeetCode/Medium/MajorityElement2.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MajorityElement2
+    {
+        public static IList<int> MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dictionary = new();
+
+            foreach (int num in nums)
+                if (dictionary.ContainsKey(num))
+                    dictionary[num]++;
+                else
+                    dictionary.Add(num, 1);
+
+            return dictionary.Where(x => x.Value > nums.Length / 3).Select(x => x.Key).ToArray();
+        }
+    }
+}

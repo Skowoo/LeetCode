4911bdcb49ceb79a9870b1033339a42b198be924
Skowoo <skowoo@tlen.pl>
2024-01-31 08:32:09 +0100

    DailyTemperatures done

---
+++ LeetCode/Medium/DailyTemperatures.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Medium
+{
+    internal class DailyTemperatures
+    {
+        public static int[] DailyTemperaturesFunc(int[] temperatures)
+        {
+            int[] result = new int[temperatures.Length];
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                result[i] = 0;
+                int counter = 0;
+                for (int j = i; j < temperatures.Length; j++)
+                {
+                    if (temperatures[j] > temperatures[i])
+                    {
+                        result[i] = counter;
+                        break;
+                    }
+                    counter++;
+                }
+            }
+
+            return result;
+        }
+    }
+}

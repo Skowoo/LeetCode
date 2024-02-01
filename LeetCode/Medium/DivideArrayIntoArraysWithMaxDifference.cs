fd9cf684aedc30b3a432502066996250ae1e3b7c
Skowoo <skowoo@tlen.pl>
2024-02-01 09:05:37 +0100

    DivideArrayIntoArraysWithMaxDifference done

---
+++ LeetCode/Medium/DivideArrayIntoArraysWithMaxDifference.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Medium
+{
+    internal class DivideArrayIntoArraysWithMaxDifference
+    {
+        public static int[][] DivideArray(int[] nums, int k)
+        {
+            Array.Sort(nums);
+
+            int[][] resultList = new int[nums.Length / 3][];
+
+            for (int i = 0; i < resultList.Length; i++)
+                resultList[i] = new int[3];
+
+            int currentFirstValue = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 3 == 0)
+                    currentFirstValue = nums[i];
+
+                if (currentFirstValue + k < nums[i])
+                    return new int[0][];
+
+                resultList[i / 3][i % 3] = nums[i];
+            }
+
+            return resultList;
+        }
+    }
+}

e3e180d4d76ef5ceb402f74fb78f03fc6dd4a8f5
Skowoo <skowoo@tlen.pl>
2024-02-05 09:00:26 +0100

    FirstUniqueCharacterInAString done

---
+++ LeetCode/Easy/FirstUniqueCharacterInAString.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal class FirstUniqueCharacterInAString
+    {
+        public static int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> kvp = new();
+
+            for (int i = 0; i < s.Length; i++)
+                if (kvp.ContainsKey(s[i]))
+                    kvp[s[i]] = -2;
+                else
+                    kvp.Add(s[i], i);
+
+            var result = kvp.Where(x => x.Value != -2);
+
+            return result.Any() ? result.First().Value : -1;
+        }
+    }
+}

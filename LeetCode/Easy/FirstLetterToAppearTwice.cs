403f54766fccceabd632461a02d772447a1d6cbd
Skowoo <skowoo@tlen.pl>
2024-02-05 09:07:02 +0100

    FirstLetterToAppearTwice done

---
+++ LeetCode/Easy/FirstLetterToAppearTwice.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal class FirstLetterToAppearTwice
+    {
+        public static char RepeatedCharacter(string s)
+        {
+            List<char> chars = new();
+            foreach (char c in s)
+                if (!chars.Contains(c))
+                    chars.Add(c);
+                else return c;
+
+            return '?';
+        }
+    }
+}

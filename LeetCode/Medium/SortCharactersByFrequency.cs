9c8f9ae3a29aa74b767fb1c4baba37a4f51d1774
Skowoo <skowoo@tlen.pl>
2024-02-05 09:16:07 +0100

    SortCharactersByFrequency done

---
+++ LeetCode/Medium/SortCharactersByFrequency.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace LeetCode.Medium
+{
+    internal class SortCharactersByFrequency
+    {
+        public static string FrequencySort(string s)
+        {
+            Dictionary<char, int> letters = new();
+
+            foreach (char c in s)
+                if (letters.ContainsKey(c))
+                    letters[c]++;
+                else
+                    letters.Add(c, 1);
+
+            StringBuilder sb = new();
+
+            foreach (var l in letters.OrderByDescending(x => x.Value))
+                for (int i = 0; i < l.Value; i++)
+                    sb.Append(l.Key);
+
+            return sb.ToString();
+        }
+    }
+}

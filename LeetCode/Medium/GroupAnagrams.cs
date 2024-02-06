61ab983c57bbde532e5adaf5b313aafc43846efc
Skowoo <skowoo@tlen.pl>
2024-02-06 10:39:17 +0100

    GroupAnagrams done

---
+++ LeetCode/Medium/GroupAnagrams.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Medium
+{
+    internal class GroupAnagrams
+    {
+        public static IList<IList<string>> GroupAnagramsFunc(string[] strs)
+        {
+            List<List<string>> result = new();
+
+            long? previousValue = null;
+            int listIndex = 0;
+
+            foreach (var word in strs.OrderBy(x => x.ToCharArray().Sum(x => (long)x * x * x * x * x)))
+            {
+                long currentValue = word.ToCharArray().Sum(x => (long)x * x * x * x * x);
+
+                if (previousValue is null)
+                {
+                    previousValue = currentValue;
+                    result.Add(new List<string>());
+                    result[listIndex].Add(word);
+                }
+                else if (previousValue != currentValue)
+                {
+                    result.Add(new List<string>());
+                    listIndex++;
+                    result[listIndex].Add(word);
+                    previousValue = currentValue;
+                }
+                else
+                {
+                    result[listIndex].Add(word);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}

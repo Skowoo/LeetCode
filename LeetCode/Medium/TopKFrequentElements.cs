4dff80c6be471fd543087cb537a0446593aecf8b
Skowoo <skowoo@tlen.pl>
2024-02-07 07:59:18 +0100

    TopKFrequentElements done

---
+++ LeetCode/Medium/TopKFrequentElements.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Medium
+{
+    internal class TopKFrequentElements
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> numberFrequency = new();
+
+            foreach (int i in nums)
+                if (numberFrequency.ContainsKey(i))
+                    numberFrequency[i]++;
+                else
+                    numberFrequency.Add(i, 1);
+
+            return numberFrequency.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray();
+        }
+    }
+}

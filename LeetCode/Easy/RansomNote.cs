31dd9f6553dd5648ba4f91d35bc8ab0746b6acc2
Skowoo <skowoo@tlen.pl>
2024-02-07 09:31:04 +0100

    RansomNote done

---
+++ LeetCode/Easy/RansomNote.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    internal class RansomNote
+    {
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> baseLetters = new();
+
+            foreach (char c in magazine)
+                if (baseLetters.ContainsKey(c))
+                    baseLetters[c]++;
+                else
+                    baseLetters.Add(c, 1);
+
+            foreach (char c in ransomNote)
+                if (baseLetters.ContainsKey(c))
+                {
+                    baseLetters[c]--;
+                    if (baseLetters[c] < 1)
+                        baseLetters.Remove(c);
+                }
+                else
+                    return false;
+
+            return true;
+        }
+    }
+}

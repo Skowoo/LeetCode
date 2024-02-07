fe6e4b34c7976610225a7df613846e856fd1138c
Skowoo <skowoo@tlen.pl>
2024-02-07 21:51:43 +0100

    KthLargestElementInAnArray done

---
+++ LeetCode/Medium/KthLargestElementInAnArray.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Medium
+{
+    internal class KthLargestElementInAnArray
+    {
+        public static int FindKthLargest(int[] nums, int k)
+        {
+            return nums.OrderByDescending(x => x).Skip(k).Take(1).Single();
+        }
+    }
+}

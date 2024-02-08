2c5ba654e99be9ea71841fa651b20208bd16cd72
Skowoo <skowoo@tlen.pl>
2024-02-08 13:30:55 +0100

    MinStack done

---
+++ LeetCode/Medium/MinStack.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Medium
+{
+    public class MinStack
+    {
+        public List<Tuple<int, int>> _values = new();
+
+        public MinStack() { }
+
+        public void Push(int val) => _values.Add(Tuple.Create(val, _values.Count + 1));
+
+        public void Pop() => _values.RemoveAt(_values.Count - 1);
+
+        public int Top() => _values[^1].Item1;
+
+        public int GetMin() => _values.Min(x => x.Item1);
+    }
+}

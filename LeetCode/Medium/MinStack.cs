7356f918299cb40fe00323f74a2a5ad818e26b4f
Skowoo <skowoo@tlen.pl>
2024-02-08 13:44:02 +0100

    MinStack second (faster) version created

--- LeetCode/Medium/MinStack.cs
+++ LeetCode/Medium/MinStack.cs
@@ -1,10 +1,40 @@
 ﻿namespace LeetCode.Medium
 {
-    public class MinStack
+    public class MinStack //Faster version using Tuple stack
+    {
+        Stack<(int value, int currentMinValue)> _stack = new();
+
+        int _minValue = int.MaxValue;
+
+        public MinStack() { }
+
+        public void Push(int val)
+        {
+            if (val < _minValue)
+                _minValue = val;
+
+            _stack.Push((val, _minValue));
+        }
+
+        public void Pop()
+        {
+            _stack.Pop();
+            if (_stack.Count == 0)
+                _minValue = int.MaxValue;
+            else
+                _minValue = _stack.Peek().currentMinValue; ;
+        }
+
+        public int Top() => _stack.Peek().value;
+
+        public int GetMin() => _stack.Peek().currentMinValue;
+    }
+
+    public class SlowMinStack // Original slow version using LINQ
     {
         public List<Tuple<int, int>> _values = new();
 
-        public MinStack() { }
+        public SlowMinStack() { }
 
         public void Push(int val) => _values.Add(Tuple.Create(val, _values.Count + 1));
 

984f6b554536da2a1245ab5fe98aed47690c2237
Skowoo <skowoo@tlen.pl>
2024-02-09 13:43:41 +0100

    ReverseWordsInAString done

---
+++ LeetCode/Medium/ReverseWordsInAString.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ReverseWordsInAString
+    {
+
+        public static string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(words);
+            return string.Join(' ', words);
+        }
+    }
+}

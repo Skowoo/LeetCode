feb3289322d2ce23f3104851a4627a63bf5f390b
Skowoo <skowoo@tlen.pl>
2024-02-10 08:25:33 +0100

    FindTheDuplicateNumber done

---
+++ LeetCode/Medium/FindTheDuplicateNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Medium
+{
+    internal class FindTheDuplicateNumber
+    {
+        public static int FindDuplicate(int[] nums)
+        {
+            List<int> ints = new();
+            foreach (int i in nums)
+                if (ints.Contains(i))
+                    return i;
+                else
+                    ints.Add(i);
+
+            return -1;
+        }
+    }
+}

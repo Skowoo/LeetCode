471d8a9b305a01154e7c26d00fa0834d611294f0
Skowoo <skowoo@tlen.pl>
2024-02-12 08:30:16 +0100

    MajorityElement done

---
+++ LeetCode/Easy/MajorityElement.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MajorityElement
+    {
+        public static int MajorityElementFunc(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+
+            Dictionary<int, int> numberCounter = new();
+
+            foreach (int num in nums)
+            {
+                if (numberCounter.ContainsKey(num))
+                {
+                    numberCounter[num]++;
+                    if (numberCounter[num] > nums.Length / 2)
+                        return num;
+                }
+                else
+                {
+                    numberCounter.Add(num, 1);
+                }
+            }
+
+            return numberCounter.OrderByDescending(x => x.Value).First().Key;
+        }
+    }
+}

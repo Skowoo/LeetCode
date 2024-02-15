316f98ce836471dae76729906a1f7e6456b1b435
Skowoo <skowoo@tlen.pl>
2024-02-15 08:19:08 +0100

    FindPolygonWithTheLargestPerimeter done

---
+++ LeetCode/Medium/FindPolygonWithTheLargestPerimeter.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Medium
+{
+    internal class FindPolygonWithTheLargestPerimeter
+    {
+
+        public static long LargestPerimeter(int[] nums)
+        {
+            nums = nums.OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentValue = nums[i];
+                nums[i] = 0;
+
+                long lefotverSum = nums.Select(x => (long)x).Sum();
+
+                if (lefotverSum > currentValue)
+                    return currentValue + lefotverSum;
+            }
+
+            return -1L;
+        }
+    }
+}
+
+//Console.WriteLine(LargestPerimeter(new int[] { 1, 12, 1, 2, 5, 50, 3 }));

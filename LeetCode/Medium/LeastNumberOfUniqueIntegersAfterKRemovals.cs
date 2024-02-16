f4a730ad0a5851108c06460129edbd132bfb2581
Skowoo <skowoo@tlen.pl>
2024-02-16 08:29:35 +0100

    LeastNumberOfUniqueIntegersAfterKRemovals done

---
+++ LeetCode/Medium/LeastNumberOfUniqueIntegersAfterKRemovals.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Medium
+{
+    internal class LeastNumberOfUniqueIntegersAfterKRemovals
+    {
+        public static int FindLeastNumOfUniqueInts(int[] arr, int k)
+        {
+            Dictionary<int, int> numsCounter = new();
+
+            foreach (var a in arr)
+                numsCounter[a] = numsCounter.ContainsKey(a) ? numsCounter[a] + 1 : 1;
+
+
+            numsCounter = numsCounter.OrderBy(s => s.Value).ToDictionary(s => s.Key, s => s.Value);
+
+            foreach (var pair in numsCounter)
+            {
+                int key = pair.Key, value = pair.Value;
+                if (k > 0)
+                {
+                    if (value <= k)
+                    {
+                        k -= value;
+                        numsCounter[key] = 0;
+                    }
+                    else
+                        break;
+
+                    if (numsCounter[key] == 0)
+                        numsCounter.Remove(key);
+                }
+                else break;
+            }
+
+            return numsCounter.Count;
+        }
+    }
+}

f47d61c0e96842d2292f445ef060b1ad509e92c0
Skowoo <skowoo@tlen.pl>
2024-02-17 09:03:06 +0100

    ExcelSheetColumnNumber done

---
+++ LeetCode/Easy/ExcelSheetColumnNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ExcelSheetColumnNumber
+    {
+
+        public static int TitleToNumber(string columnTitle) // -64
+        {
+            int result = 0;
+            for (int i = 0; i < columnTitle.Length; i++)
+                result = result * 26 + columnTitle[i] - 64;
+
+            return result;
+        }
+    }
+}
+
+//Console.WriteLine(TitleToNumber("ZY"));
+//Console.WriteLine(TitleToNumber("AB"));
+//Console.WriteLine(TitleToNumber("A"));

976e864b2212ab085adcd6a2c543c10806d771d7
Skowoo <skowoo@tlen.pl>
2024-02-18 09:26:45 +0100

    BuyTwoChocolates done

---
+++ LeetCode/Easy/BuyTwoChocolates.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Easy
+{
+    internal class BuyTwoChocolates
+    {
+        public static int BuyChoco(int[] prices, int money)
+        {
+            var twoChocos = prices.OrderBy(x => x).Take(2).Sum();
+            if (twoChocos <= money)
+                return money - twoChocos;
+            else return money;
+        }
+    }
+}

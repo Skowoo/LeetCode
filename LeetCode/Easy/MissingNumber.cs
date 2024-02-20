ef21edf7410c19378852bfd958740101c5645e11
Skowoo <skowoo@tlen.pl>
2024-02-20 07:01:24 +0100

    MissingNumber done

---
+++ LeetCode/Easy/MissingNumber.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MissingNumber
+    {
+        public static int MissingNumberFunc(int[] nums) => nums.Length * (nums.Length + 1) / 2 - nums.Sum();
+    }
+}

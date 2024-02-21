a366a64f87cf19d0f92605bb4b6597b5b86bb8ad
Skowoo <skowoo@tlen.pl>
2024-02-21 08:34:13 +0100

    BitwiseANDofNumbersRange done

---
+++ LeetCode/Medium/BitwiseANDofNumbersRange.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Medium
+{
+    internal class BitwiseANDofNumbersRange
+    {
+        public static int RangeBitwiseAnd(int left, int right)
+        {
+            while (left < right)
+                right &= (right - 1);
+
+            return left & right;
+        }
+    }
+}

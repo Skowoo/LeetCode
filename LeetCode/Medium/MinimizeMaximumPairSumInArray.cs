002f14b152d79bf10aaabc0415bb446656445ed4
Skowoo <skowoo@tlen.pl>
2024-02-23 08:55:36 +0100

    MinimizeMaximumPairSumInArray done

---
+++ LeetCode/Medium/MinimizeMaximumPairSumInArray.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimizeMaximumPairSumInArray
+    {
+        public static int MinPairSum(int[] nums)
+        {
+            int result = 0;
+
+            var orderedArray = nums.OrderBy(x => x).ToArray();
+
+            for (int i = 0; i < orderedArray.Length / 2; i++)
+                if (orderedArray[i] + orderedArray[orderedArray.Length - i - 1] > result)
+                    result = orderedArray[i] + orderedArray[orderedArray.Length - i - 1];
+
+            return result;
+        }
+    }
+}

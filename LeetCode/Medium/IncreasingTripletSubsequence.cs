a97da7640d9a3455aa71584258d591be1976e79a
Skowoo <skowoo@tlen.pl>
2024-02-23 15:25:47 +0100

    IncreasingTripletSubsequence done

---
+++ LeetCode/Medium/IncreasingTripletSubsequence.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Medium
+{
+    internal class IncreasingTripletSubsequence
+    {
+        public static bool IncreasingTriplet(int[] nums)
+        {
+            int first = nums[0];
+            int second = int.MaxValue;
+
+            foreach (int x in nums)
+                if (first < second && second < x)
+                    return true;
+                else
+                    if (first < x && second > x)
+                    second = x;
+                else if (first > x)
+                    first = x;
+
+            return false;
+        }
+    }
+}

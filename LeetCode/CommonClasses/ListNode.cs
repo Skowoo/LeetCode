65d42e4858ec34c25f9f0bdaec3c4d88a9137c73
Skowoo <skowoo@tlen.pl>
2024-02-26 09:19:19 +0100

    ListNode added do CommonClasses

---
+++ LeetCode/CommonClasses/ListNode.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+
+namespace LeetCode.CommonClasses
+{
+    public class ListNode
+    {
+        public int val;
+
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

e5cb92bba2cc18dba4c62c41bb14653011d47474
Skowoo <skowoo@tlen.pl>
2024-02-27 09:38:21 +0100

    BinaryTreePostorderTraversal done

---
+++ LeetCode/Easy/BinaryTreePostorderTraversal.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class BinaryTreePostorderTraversal
+    {
+        public static IList<int> PostorderTraversal(TreeNode root)
+        {
+            List<int> result = new();
+
+            if (root is not null)
+                PostOrder(root);
+
+            void PostOrder(TreeNode root)
+            {
+                if (root.left is not null)
+                    PostOrder(root.left);
+                if (root.right is not null)
+                    PostOrder(root.right);
+
+                result.Add(root.val);
+            }
+
+            return result;
+        }
+    }
+}

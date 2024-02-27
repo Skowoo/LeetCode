77082993517f13a70a6a66cf61080c66467c64ac
Skowoo <skowoo@tlen.pl>
2024-02-27 10:00:48 +0100

    InvertBinaryTree done

---
+++ LeetCode/Easy/InvertBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class InvertBinaryTree
+    {
+        public static TreeNode? InvertTree(TreeNode root)
+        {
+            if (root is null)
+                return null;
+
+            Reverse(root);
+
+            static void Reverse(TreeNode node)
+            {
+                // Tuple reverse
+                (node.right, node.left) = (node.left, node.right);
+
+                if (node.left is not null)
+                    Reverse(node.left);
+
+                if (node.right is not null)
+                    Reverse(node.right);
+            }
+
+            return root;
+        }
+    }
+}

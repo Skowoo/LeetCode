3566aa77b8d9a26f820e2bb2403e51c10c39d59e
Skowoo <skowoo@tlen.pl>
2024-02-27 15:34:47 +0100

    ApplyDiscountToPrices done

---
+++ LeetCode/Medium/ApplyDiscountToPrices.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ApplyDiscountToPrices
+    {
+
+        public static string DiscountPrices(string sentence, int discount)
+        {
+            var words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+                if (words[i].StartsWith('$') && words[i].Length <= 11 && long.TryParse(words[i].Remove(0, 1), out long parsedValue))
+                {
+                    var result = parsedValue * (1 - discount / 100D);
+                    words[i] = "$" + result.ToString("F2");
+                }
+
+            return string.Join(' ', words);
+        }
+    }
+}
+
+// Console.WriteLine(DiscountPrices("there are $1 $2 and 5$ an$100 an$100  $111 $1123gf  candies in the shop", 50));
+
+// Not-used REGEX for valid price: @"\B\$\d{1,10}\b"

a692997362340f224b5bdbab2dd7ccabc8291ae1
Skowoo <skowoo@tlen.pl>
2024-02-27 10:53:29 +0100

    MergeStringsAlternately done

---
+++ LeetCode/Easy/MergeStringsAlternately.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MergeStringsAlternately
+    {
+
+        public static string MergeAlternately(string word1, string word2)
+        {
+            string result = string.Empty;
+
+            int wordsIndexer = 0;
+
+            while (true)
+            {
+                if (word1.Length > wordsIndexer)
+                    result += word1[wordsIndexer];
+
+                if (word2.Length > wordsIndexer)
+                    result += word2[wordsIndexer];
+
+                wordsIndexer++;
+
+                if (wordsIndexer >= Math.Max(word1.Length, word2.Length))
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
+
+// Console.WriteLine(MergeAlternately("abcde","123456789"));

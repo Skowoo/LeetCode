ee5ee1e5a4a4b8d45139ddbb75d41d5bbbd79629
Skowoo <skowoo@tlen.pl>
2024-02-27 10:03:37 +0100

    BinaryTreePreorderTraversal done

---
+++ LeetCode/Easy/BinaryTreePreorderTraversal.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class BinaryTreePreorderTraversal
+    {
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            List<int> result = new();
+
+            if (root is not null)
+                Preorder(root);
+
+            void Preorder(TreeNode root)
+            {
+                result.Add(root.val);
+
+                if (root.left is not null)
+                    Preorder(root.left);
+                if (root.right is not null)
+                    Preorder(root.right);
+            }
+
+            return result;
+        }
+    }
+}

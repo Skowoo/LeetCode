36578bac3f9cd6bc914eb2be6b180c6af7748d21
Skowoo <skowoo@tlen.pl>
2024-02-28 12:49:14 +0100

    BreakAPalindrome done

---
+++ LeetCode/Medium/BreakAPalindrome.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCode.Medium
+{
+    internal class BreakAPalindrome
+    {
+        public static string BreakPalindrome(string palindrome)
+        {
+            if (palindrome.Length == 1)
+                return string.Empty;
+
+            StringBuilder sb = new StringBuilder(palindrome);
+
+            for (int i = 0; i < palindrome.Length / 2; i++)
+            {
+                if (palindrome[i] != 'a')
+                {
+                    sb[i] = 'a';
+                    return sb.ToString();
+                }
+            }
+
+            sb[sb.Length - 1] = 'b';
+
+            return sb.ToString();
+        }
+    }
+}
+
+// Console.WriteLine(BreakPalindrome("a"));

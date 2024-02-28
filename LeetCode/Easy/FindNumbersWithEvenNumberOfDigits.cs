daa4a1832f67c54132f0d38480d6272189065e99
Skowoo <skowoo@tlen.pl>
2024-02-28 15:10:52 +0100

    FindNumbersWithEvenNumberOfDigits done

---
+++ LeetCode/Easy/FindNumbersWithEvenNumberOfDigits.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal class FindNumbersWithEvenNumberOfDigits
+    {
+        public static int FindNumbers(int[] nums)
+        {
+            int counter = 0;
+
+            foreach (int x in nums)
+                if (x.ToString().Length % 2 == 0)
+                    counter++;
+
+            return counter;
+        }
+    }
+}

adee1269e73a7749cfb09cd86ee3041c8f09a0c7
Skowoo <skowoo@tlen.pl>
2024-02-28 09:47:46 +0100

    FindBottomLeftTreeValue done

---
+++ LeetCode/Medium/FindBottomLeftTreeValue.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal class FindBottomLeftTreeValue
+    {
+        public static int FindBottomLeftValue(TreeNode root)
+        {
+            List<TreeNode> nodeLevels = new() { root };
+
+            while (true)
+            {
+                List<TreeNode> nextLevel = new();
+
+                foreach (TreeNode node in nodeLevels)
+                {
+                    if (node.left is not null)
+                        nextLevel.Add(node.left);
+
+                    if (node.right is not null)
+                        nextLevel.Add(node.right);
+                }
+
+                if (nextLevel.Count == 0)
+                    break;
+
+                nodeLevels = nextLevel;
+            }
+
+            return nodeLevels.First().val;
+        }
+    }
+}

52e05a7a7c2958aff5e70c1116c45fcb94a34de8
Skowoo <skowoo@tlen.pl>
2024-02-28 10:48:31 +0100

    FindThePrefixCommonArrayOfTwoArrays done

---
+++ LeetCode/Medium/FindThePrefixCommonArrayOfTwoArrays.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Medium
+{
+    internal class FindThePrefixCommonArrayOfTwoArrays
+    {
+        public static int[] FindThePrefixCommonArray(int[] A, int[] B)
+        {
+            int[] result = new int[A.Length];
+
+            for (int i = 0; i < A.Length; i++)
+                result[i] = A.Take(i + 1).Intersect(B.Take(i + 1)).Count();
+
+            return result;
+        }
+    }
+}

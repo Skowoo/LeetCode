c1afc1d385ae0798ae031dff989c3b1aff822264
Skowoo <skowoo@tlen.pl>
2024-02-28 11:55:36 +0100

    SplitStringsBySeparator done

---
+++ LeetCode/Easy/SplitStringsBySeparator.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal class SplitStringsBySeparator
+    {
+        public static IList<string> SplitWordsBySeparator(IList<string> words, char separator)
+        {
+            List<string> result = new();
+
+            foreach (var item in words)
+                result.AddRange(item.Split(separator, StringSplitOptions.RemoveEmptyEntries));
+
+            return result;
+        }
+    }
+}
+
+
+// var result = SplitWordsBySeparator(new[] { "one.two.three", "four.five", "six" }, '.');
+
+// foreach (var word in result)
+//     Console.WriteLine(word);

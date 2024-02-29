e1105eab6c5b05dfbd994fa0262eb1f5c9e3dd49
Skowoo <skowoo@tlen.pl>
2024-02-29 10:01:17 +0100

    RemoveNthNodeFromEndOfList done

---
+++ LeetCode/Medium/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,40 @@
+﻿#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CS8603 // Possible null reference return.
+
+
+namespace LeetCode.Medium
+{
+    internal class RemoveNthNodeFromEndOfList
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head.next is null)
+                return null;
+
+            ListNode newTempHead = new() { val = -1, next = head };
+
+            int totalLength = 0;
+            head = newTempHead;
+            while (head.next is not null)
+            {
+                head = head.next;
+                totalLength++;
+            }
+            head = newTempHead;
+
+            int targetIndex = totalLength - n;
+
+            for (int i = 0; i < targetIndex; i++)
+                head = head.next;
+
+            if (head.next is null)
+                return newTempHead.next;
+            if (head.next.next is null)
+                head.next = null;
+            else
+                head.next = head.next.next;
+
+            return newTempHead.next;
+        }
+    }
+}

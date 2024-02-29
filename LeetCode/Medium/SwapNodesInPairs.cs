21fd5754ae6188ce947dbe0fdb1a52486c84642a
Skowoo <skowoo@tlen.pl>
2024-02-29 13:23:24 +0100

    SwapNodesInPairs done

---
+++ LeetCode/Medium/SwapNodesInPairs.cs
@@ -0,0 +1,54 @@
+﻿#pragma warning disable CS8603 // Possible null reference return.
+#pragma warning disable CS8601 // Possible null reference assignment.
+
+
+namespace LeetCode.Medium
+{
+    internal class SwapNodesInPairs
+    {
+        public static ListNode SwapPairs(ListNode head)
+        {
+            if (head is null)
+                return null;
+
+            ListNode newHead = new() { val = -1, next = head };
+            head = newHead;
+
+            while (true)
+            {
+                ListNode? first, second, third;
+
+                if (head.next is not null)
+                    first = head.next;
+                else
+                    first = null;
+
+                if (head.next is not null && head.next.next is not null)
+                    second = head.next.next;
+                else
+                    second = null;
+
+                if (head.next is not null && head.next.next is not null && head.next.next.next is not null)
+                    third = head.next.next.next;
+                else
+                    third = null;
+
+                if (first is null || second is null)
+                    break;
+                else
+                {
+                    first.next = third;
+                    second.next = first;
+                    head.next = second;
+                }
+
+                if (third is null)
+                    break;
+
+                head = first;
+            }
+
+            return newHead.next;
+        }
+    }
+}

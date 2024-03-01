3cd8f37754988b68e2e8f0c60438f9fa26062319
Skowoo <skowoo@tlen.pl>
2024-03-01 09:38:11 +0100

    PartitioningIntoMinimumNumberOfDeci_BinaryNumbers done

---
+++ LeetCode/Medium/PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Medium
+{
+    internal class PartitioningIntoMinimumNumberOfDeci_BinaryNumbers
+    {
+        public static int MinPartitions(string n)
+        {
+            char max = '0';
+            foreach (char i in n)
+            {
+                if (i > max)
+                    max = i;
+                if (max == '9')
+                    return 9;
+            }
+
+            return int.Parse(max.ToString());
+        }
+
+        //Faster
+        public static int MinPartitionsLambda(string n) => n.Max(x => (int)x) - 64;
+    }
+}

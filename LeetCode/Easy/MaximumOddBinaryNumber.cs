d2b373b7ae72c8fd4155db72cc0248a9a1346a98
Skowoo <skowoo@tlen.pl>
2024-03-01 07:58:41 +0100

    MaximumOddBinaryNumber done

---
+++ LeetCode/Easy/MaximumOddBinaryNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MaximumOddBinaryNumber
+    {
+        public static string MaximumOddBinaryNumberFunc(string s)
+        {
+            var array = s.ToCharArray();
+            int onesCount = array.Where(x => x == '1').Count();
+            int zerosCount = array.Length - onesCount;
+            if (onesCount == 1)
+                return "1";
+
+            string result = string.Empty;
+            for (int i = 0; i < onesCount - 1; i++)
+                result += '1';
+
+            for (int i = 0; i < zerosCount; i++)
+                result += '0';
+
+            return result + '1';
+        }
+    }
+}

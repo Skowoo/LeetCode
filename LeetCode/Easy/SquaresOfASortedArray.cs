3f527f87c4d1ebaf7c56a42c6afaa240f64086b8
Skowoo <skowoo@tlen.pl>
2024-03-02 07:44:00 +0100

    SquaresOfASortedArray done

---
+++ LeetCode/Easy/SquaresOfASortedArray.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Easy
+{
+    internal class SquaresOfASortedArray
+    {
+        public static int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = (int)Math.Pow(nums[i], 2);
+            Array.Sort(nums);
+            return nums;
+        }
+    }
+}

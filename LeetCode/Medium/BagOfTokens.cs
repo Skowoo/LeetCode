21839818078596c74619a65b26e5e8df5af47146
Skowoo <skowoo@tlen.pl>
2024-03-04 08:55:07 +0100

    BagOfTokens done

---
+++ LeetCode/Medium/BagOfTokens.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Medium
+{
+    internal class BagOfTokens
+    {
+        public static int BagOfTokensScore(int[] tokens, int power)
+        {
+            Array.Sort(tokens);
+
+            int smallest = 0, biggest = tokens.Length - 1, result = 0;
+
+            while (smallest <= biggest)
+            {
+                if (power >= tokens[smallest])
+                {
+                    power -= tokens[smallest++];
+                    result++;
+                }
+                else
+                {
+                    if (result == 0 || smallest == biggest)
+                        break;
+                    power += tokens[biggest--];
+                    result--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
+// Console.WriteLine(BagOfTokensScore(new int[] { 200, 100}, 150));

ee7a18425b1bbb736448ff929f5a58b0b33254a3
Skowoo <skowoo@tlen.pl>
2024-03-06 08:25:13 +0100

    LinkedListCycle done

---
+++ LeetCode/Easy/LinkedListCycle.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class LinkedListCycle
+    {
+        // Extremely uneffective solution - beats 5% :<
+        public static bool HasCycleBAD(ListNode head)
+        {
+            List<ListNode> visitedNodes = new();
+
+            while (head.next is not null)
+            {
+                if (visitedNodes.Contains(head.next))
+                    return true;
+                visitedNodes.Add(head.next);
+
+                head = head.next;
+            }
+
+            return false;
+        }
+    }
+}

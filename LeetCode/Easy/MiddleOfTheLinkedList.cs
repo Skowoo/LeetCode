1db53f386b466d2fb4b9ed9b201d5baa264645e2
Skowoo <skowoo@tlen.pl>
2024-03-07 11:09:28 +0100

    MiddleOfTheLinkedList - two pointer solution added

--- LeetCode/Easy/MiddleOfTheLinkedList.cs
+++ LeetCode/Easy/MiddleOfTheLinkedList.cs
@@ -4,7 +4,24 @@ namespace LeetCode.Easy
 {
     internal class MiddleOfTheLinkedList
     {
-        public static ListNode MiddleNode(ListNode head)
+        public static ListNode MiddleNodeTwoPointer(ListNode head)
+        {
+            if (head is null || head.next is null)
+                return head!;
+
+            ListNode slow = head;
+            ListNode fast = head.next;
+
+            while (fast.next is not null && fast.next.next is not null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow.next;
+        }
+
+        public static ListNode MiddleNodeCounter(ListNode head)
         {
             ListNode oriHead = head;
             int counter = 0;

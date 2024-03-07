f84bc3809cd39c85aeca556b646c897f63325d96
Skowoo <skowoo@tlen.pl>
2024-03-07 06:53:28 +0100

    MiddleOfTheLinkedList done

---
+++ LeetCode/Easy/MiddleOfTheLinkedList.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class MiddleOfTheLinkedList
+    {
+        public static ListNode MiddleNode(ListNode head)
+        {
+            ListNode oriHead = head;
+            int counter = 0;
+
+            while (head.next is not null)
+            {
+                counter++;
+                head = head.next;
+            }
+
+            for (int i = 0; i < Math.Ceiling(1.0D * counter / 2); i++)
+                oriHead = oriHead.next;
+
+            return oriHead;
+        }
+    }
+}

a71c1c1ea1a4ce9c45e41ac6ac8091e70eb6fc2e
Skowoo <skowoo@tlen.pl>
2024-03-08 06:51:12 +0100

    CountElementsWithMaximumFrequency done

---
+++ LeetCode/Easy/CountElementsWithMaximumFrequency.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    internal class CountElementsWithMaximumFrequency
+    {
+        public static int MaxFrequencyElements(int[] nums)
+        {
+            Dictionary<int, int> numbersCounter = new();
+
+            foreach (int num in nums)
+                if (numbersCounter.ContainsKey(num))
+                    numbersCounter[num]++;
+                else
+                    numbersCounter.Add(num, 1);
+
+            return numbersCounter.Where(x => x.Value == numbersCounter.Values.Max()).Select(x => x.Value).Sum();
+        }
+    }
+}

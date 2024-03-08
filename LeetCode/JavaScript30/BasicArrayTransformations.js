381b87a36e8b1085608fbd49519be0de67eaa320
Skowoo <skowoo@tlen.pl>
2024-03-08 14:51:02 +0100

    JS30 - first tasks done

---
+++ LeetCode/JavaScript30/BasicArrayTransformations.js
@@ -0,0 +1,32 @@
+﻿
+// Apply Transform Over Each Element in Array
+
+var map = function (arr, fn) {
+    var resultList = []
+    for (var i = 0; i < arr.length; i++)
+        resultList.push(fn(arr[i], i))
+    return resultList
+};
+
+
+// Filter Elements from Array
+
+var filter = function (arr, fn) {
+    var resultArray = []
+    for (var i = 0; i < arr.length; i++)
+        if (fn(arr[i], i))
+            resultArray.push(arr[i])
+    return resultArray
+};
+
+
+
+// Array Reduce Transformation
+
+var reduce = function (nums, fn, init) {
+    var accum = init
+    nums.forEach(function (item) {
+        accum = fn(accum, item)
+    })
+    return accum
+};
---
+++ LeetCode/JavaScript30/Closures.js
@@ -0,0 +1,47 @@
+﻿var createHelloWorld = function () {
+    return function (...args) {
+        return "Hello World"
+    }
+};
+
+var createCounter = function (n) {
+    var curVal = n
+    return function () {
+        return curVal++
+    };
+};
+
+var expect = function (val) {
+    return {
+        toBe: function (n) {
+            if (val === n)
+                return true
+            else
+                throw new Error("Not Equal")
+        },
+        notToBe: function (n) {
+            if (val !== n)
+                return true
+            else
+                throw new Error("Equal")
+        }
+    }
+};
+
+var createCounter = function (init) {
+    var initialValue = init
+    var currentValue = init
+
+    return {
+        increment: function () {
+            return ++currentValue
+        },
+        decrement: function () {
+            return --currentValue
+        },
+        reset: function () {
+            currentValue = initialValue
+            return currentValue
+        }
+    }
+};

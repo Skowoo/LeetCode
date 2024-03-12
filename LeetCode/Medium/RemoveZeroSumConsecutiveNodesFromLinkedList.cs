c2ff593e939fb016fc00d7ee6c4be8d08b221dee
Skowoo <skowoo@tlen.pl>
2024-03-12 11:44:11 +0100

    RemoveZeroSumConsecutiveNodesFromLinkedList cleanup

--- LeetCode/Medium/RemoveZeroSumConsecutiveNodesFromLinkedList.cs
+++ LeetCode/Medium/RemoveZeroSumConsecutiveNodesFromLinkedList.cs
@@ -1,5 +1,4 @@
 ﻿#pragma warning disable CS8603 // Possible null reference return.
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
 
 
 using LeetCode.CommonClasses;
@@ -7,33 +6,6 @@ using LeetCode.CommonClasses;
 namespace LeetCode.Medium
 {
     internal class RemoveZeroSumConsecutiveNodesFromLinkedList
-    {
-        public static ListNode RemoveZeroSumSublists(ListNode head)
-        {
-            List<int> listedInput = new();
-            while (head is not null)
-            {
-                listedInput.Add(head.val);
-                head = head.next;
-            }
-
-            // TBD: remove subarrays which sums to 0
-
-
-            ListNode result = new(2000, null);
-            ListNode temp = result;
-            foreach (int i in listedInput)
-            {
-                ListNode newNode = new(i, null);
-                temp.next = newNode;
-                temp = temp.next;
-            }
-
-            return result.next;
-        }
-    }
-
-    internal class RemoveZeroSumConsecutiveNodesFromLinkedList_InListApproach
     {
         public static ListNode RemoveZeroSumSublists(ListNode head)
         {

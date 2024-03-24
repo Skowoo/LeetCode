b9955f4c6332910fed41246a406908450caa6c80
Skowoo <skowoo@tlen.pl>
2024-03-24 07:54:37 +0100

    FindTheDuplicateNumber - better solution

--- LeetCode/Medium/FindTheDuplicateNumber.cs
+++ LeetCode/Medium/FindTheDuplicateNumber.cs
@@ -4,6 +4,29 @@
     {
         public static int FindDuplicate(int[] nums)
         {
+            int tortoise = nums[0], hare = nums[0];
+            while (true)
+            {
+                tortoise = nums[tortoise];
+                hare = nums[nums[hare]];
+                if (tortoise == hare)
+                    break;
+            }
+
+            int firstPointer = nums[0];
+            int secondPointer = tortoise;
+
+            while(firstPointer != secondPointer)
+            {
+                firstPointer = nums[firstPointer];
+                secondPointer = nums[secondPointer];
+            }
+
+            return firstPointer;
+        }
+
+        public static int FindDuplicateBrutal(int[] nums)
+        {
             List<int> ints = new();
             foreach (int i in nums)
                 if (ints.Contains(i))

c44381547b03f5c4015432bb8002b795801d6b58
Skowoo <skowoo@tlen.pl>
2024-03-29 08:29:45 +0100

    CountSubarraysWhereMaxElementAppearsAtLeastKTimes done

---
+++ LeetCode/Medium/CountSubarraysWhereMaxElementAppearsAtLeastKTimes.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Medium
+{
+    internal class CountSubarraysWhereMaxElementAppearsAtLeastKTimes
+    {
+        public static long CountSubarrays(int[] nums, int k)
+        {
+            long result = 0L;
+            int leftPointer = 0,
+                rightPointer = 0,
+                maxValue = nums.Max(),
+                maxCounter = 0;
+
+            while (rightPointer < nums.Length)
+            {
+                if (nums[rightPointer] == maxValue)
+                    maxCounter++;
+
+                while (maxCounter >= k)
+                {
+                    if (nums[leftPointer] == maxValue)
+                        maxCounter--;
+
+                    leftPointer++;
+                }
+
+                result += leftPointer;
+                rightPointer++;
+            }
+            return result;
+        }
+    }
+}

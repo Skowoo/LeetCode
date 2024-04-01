c4bf6767e92b50d0dc9024a271e6c541a35d5f11
Skowoo <skowoo@tlen.pl>
2024-04-01 10:53:53 +0200

    ReverseOnlyLetters done

---
+++ LeetCode/Easy/ReverseOnlyLetters.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal class ReverseOnlyLetters
+    {
+        public static string ReverseOnlyLetters__FUNC(string s)
+        {
+            List<char> letters = new();
+            List<int> indexes = new();
+            for (int i = 0; i < s.Length; i++)
+                if (char.IsLetter(s[i]))
+                {
+                    indexes.Add(i);
+                    letters.Add(s[i]);
+                }
+
+            letters.Reverse();
+            StringBuilder sb = new(s);
+
+            foreach (int i in indexes)
+            {
+                char target = letters.First();
+                sb[i] = target;
+                letters.Remove(target);
+            }
+
+            return sb.ToString();
+        }
+    }
+}

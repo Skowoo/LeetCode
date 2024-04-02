90358e5d51077e5d7643e70ee39097e373661aec
Skowoo <skowoo@tlen.pl>
2024-04-02 07:54:30 +0200

    IsomorphicStrings done

---
+++ LeetCode/Easy/IsomorphicStrings.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy
+{
+    internal class IsomorphicStrings
+    {
+        public static bool IsIsomorphic(string s, string t)
+        {
+            Dictionary<char, char> letterPairs = new();
+            for (int i = 0; i < s.Length; i++)
+                if (letterPairs.ContainsKey(s[i]))
+                {
+                    if (letterPairs[s[i]] != t[i])
+                        return false;
+                }
+                else
+                    letterPairs.Add(s[i], t[i]);
+
+            letterPairs.Clear();
+            for (int i = 0; i < s.Length; i++)
+                if (letterPairs.ContainsKey(t[i]))
+                {
+                    if (letterPairs[t[i]] != s[i])
+                        return false;
+                }
+                else
+                    letterPairs.Add(t[i], s[i]);
+
+            return true;
+        }
+    }
+}

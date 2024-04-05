c82bc094b6aeca71c8cb039966e334098f304aa0
Skowoo <skowoo@tlen.pl>
2024-04-05 07:57:32 +0200

    MakeTheStringGreat done

---
+++ LeetCode/Easy/MakeTheStringGreat.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MakeTheStringGreat
+    {
+        public static string MakeGood(string s)
+        {
+            List<char> chars = new(s);
+            bool altered = true;
+            while (altered)
+            {
+                altered = false;
+                for (int i = 1; i < chars.Count; i++)
+                    if (chars[i] == (chars[i - 1] - 32) || chars[i - 1] == (chars[i] - 32))
+                    {
+                        chars.RemoveAt(i);
+                        chars.RemoveAt(i - 1);
+                        altered = true;
+                        break;
+                    }
+            }
+            return new string(chars.ToArray());
+        }
+    }
+}

367d392c58f05bf52e8c3b4f98ba0ba7bb31e064
Skowoo <skowoo@tlen.pl>
2024-04-06 11:46:36 +0200

    MinimumRemoveToMakeValidParentheses done

---
+++ LeetCode/Medium/MinimumRemoveToMakeValidParentheses.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCode.Medium
+{
+    internal class MinimumRemoveToMakeValidParentheses
+    {
+        public static string MinRemoveToMakeValid(string s)
+        {
+            Stack<int> openingsStack = new();
+            List<int> notClosedIndexes = new();
+
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] == '(')
+                    openingsStack.Push(i);
+                else if (s[i] == ')')
+                    if (!openingsStack.TryPop(out var _))
+                        notClosedIndexes.Add(i);
+
+            StringBuilder result = new();
+
+            foreach (int item in openingsStack)
+                notClosedIndexes.Add(item);
+
+            for (int i = 0; i < s.Length; i++)
+                if (!notClosedIndexes.Contains(i))
+                    result.Append(s[i]);
+
+            return result.ToString();
+        }
+    }
+}

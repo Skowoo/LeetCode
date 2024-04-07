01701b2c3b901bdf38c7ec8aa63403bc1488da47
Skowoo <skowoo@tlen.pl>
2024-04-07 07:51:40 +0200

    ValidParenthesisString done

---
+++ LeetCode/Medium/ValidParenthesisString.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ValidParenthesisString
+    {
+        public static bool CheckValidString(string s)
+        {
+            int minCounter = 0, maxCounter = 0;
+
+            foreach (char c in s)
+            {
+                minCounter += c == '(' ? 1 : -1;
+                maxCounter += c != ')' ? 1 : -1;
+
+                if (maxCounter < 0)
+                    break;
+                minCounter = Math.Max(minCounter, 0);
+            }
+
+            return minCounter == 0;
+        }
+    }
+}

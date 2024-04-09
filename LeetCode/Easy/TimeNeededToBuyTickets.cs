0060577daa4420777ec35705978347678c60a80f
Skowoo <skowoo@tlen.pl>
2024-04-09 06:41:43 +0200

    TimeNeededToBuyTickets done

---
+++ LeetCode/Easy/TimeNeededToBuyTickets.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal class TimeNeededToBuyTickets
+    {
+        public static int TimeRequiredToBuy(int[] tickets, int k)
+        {
+            int timer = 0;
+            while (true)
+                for (int i = 0; i < tickets.Length; i++)
+                    if (tickets[i] > 0)
+                    {
+                        timer++;
+                        tickets[i]--;
+
+                        if (i == k && tickets[i] == 0)
+                            return timer;
+                    }
+        }
+    }
+}

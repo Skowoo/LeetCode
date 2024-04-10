c6f4a373aac900a0979c9a622b0d99ec62e041b1
Skowoo <skowoo@tlen.pl>
2024-04-10 07:26:03 +0200

    RevealCardsInIncreasingOrder done

---
+++ LeetCode/Medium/RevealCardsInIncreasingOrder.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Medium
+{
+    internal class RevealCardsInIncreasingOrder
+    {
+        public static int[] DeckRevealedIncreasing(int[] deck)
+        {
+            Array.Sort(deck);
+            int[] result = new int[deck.Length];
+            Queue<int> queue = new(Enumerable.Range(0, deck.Length));
+
+            for (int i = 0; i < deck.Length; i++)
+            {
+                result[queue.Dequeue()] = deck[i];
+                if (queue.Count > 0)
+                    queue.Enqueue(queue.Dequeue());
+            }
+            return result;
+        }
+    }
+}

bb44cf306b40e3c4d1cb7816a5cbe8ef33d0017e
Skowoo <skowoo@tlen.pl>
2024-04-11 08:52:21 +0200

    RemoveKDigits done

---
+++ LeetCode/Medium/RemoveKDigits.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LeetCode.Medium
+{
+    internal class RemoveKDigits
+    {
+        public static string RemoveKdigits(string num, int k)
+        {
+            if (num.Length == k)
+                return "0";
+
+            StringBuilder result = new();
+
+            foreach (char c in num)
+            {
+                while (k > 0 && result.Length > 0 && result[^1] > c)
+                {
+                    result.Remove(result.Length - 1, 1);
+                    k--;
+                }
+                result.Append(c);
+            }
+
+            while (k > 0)
+            {
+                result.Remove(result.Length - 1, 1);
+                k--;
+            }
+
+            while (result.Length > 1 && result[0] == '0')
+                result.Remove(0, 1);
+
+            return result.ToString();
+        }
+    }
+}

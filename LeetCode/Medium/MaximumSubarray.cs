12b4cad8f63fdd20966020a9e051638821891150
Skowoo <skowoo@tlen.pl>
2024-04-11 18:19:04 +0200

    MaximumSubarray done

---
+++ LeetCode/Medium/MaximumSubarray.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MaximumSubarray
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            int maxSum = int.MinValue;
+            int currentSum = 0;
+
+            foreach (int x in nums)
+            {
+                currentSum += x;
+                maxSum = Math.Max(maxSum, currentSum);
+                if (currentSum < 0)
+                    currentSum = 0;
+            }
+            return maxSum;
+        }
+    }
+}

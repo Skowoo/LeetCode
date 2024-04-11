b2ff26398b6f10d060e2b6748899462c7e15fd52
Skowoo <skowoo@tlen.pl>
2024-04-11 20:56:22 +0200

    JumpGame done

---
+++ LeetCode/Medium/JumpGame.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Medium
+{
+    internal class JumpGame
+    {
+        public static bool CanJump(int[] nums)
+        {
+            int lastAccesibleIndex = nums.Length - 1;
+
+            for (int i = lastAccesibleIndex; i >= 0; i--)
+                if (i + nums[i] >= lastAccesibleIndex)
+                    lastAccesibleIndex = i;
+
+            return lastAccesibleIndex == 0;
+        }
+    }
+}

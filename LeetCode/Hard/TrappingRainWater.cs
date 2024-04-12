89538bf516d6760ca853e79c70933c88bc4e635e
Skowoo <skowoo@tlen.pl>
2024-04-12 09:23:43 +0200

    TrappingRainWater done

---
+++ LeetCode/Hard/TrappingRainWater.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Hard
+{
+    internal class TrappingRainWater
+    {
+        public static int Trap(int[] height)
+        {
+            int borderSize = 0,
+                leftBorderIndex = 0,
+                totalQuantity = 0,
+                currentQuantity = 0;
+
+            for (int i = 0; i < height.Length; i++)
+            {
+                if (height[i] < borderSize)
+                {
+                    currentQuantity += borderSize - height[i];
+                }
+                else if (height[i] >= borderSize)
+                {
+                    totalQuantity += currentQuantity;
+                    currentQuantity = 0;
+                    borderSize = height[i];
+                    leftBorderIndex = i;
+                }
+            }
+
+            currentQuantity = 0;
+            borderSize = 0;
+
+            for (int i = height.Length - 1; i >= leftBorderIndex; i--)
+            {
+                if (height[i] < borderSize)
+                {
+                    currentQuantity += borderSize - height[i];
+                }
+                else if (height[i] >= borderSize)
+                {
+                    totalQuantity += currentQuantity;
+                    currentQuantity = 0;
+                    borderSize = height[i];
+                }
+            }
+
+            return totalQuantity;
+        }
+    }
+}

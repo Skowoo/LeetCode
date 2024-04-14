e30bd26ca15ae9d744cd262270c3e6a48b9373da
Skowoo <skowoo@tlen.pl>
2024-04-14 08:45:06 +0200

    SumOLeftLeaves done

---
+++ LeetCode/Easy/SumOLeftLeaves.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class SumOLeftLeaves
+    {
+        public static int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root is null)
+                return 0;
+
+            int result = 0;
+
+            Recurse(root);
+            void Recurse(TreeNode node)
+            {
+                if (node.left is not null)
+                    Recurse(node.left);
+
+                if (node.right is not null)
+                    Recurse(node.right);
+
+                if (node.left is not null && node.left.right is null && node.left.left is null)
+                    result += node.left.val;
+            }
+
+            return result;
+        }
+    }
+}

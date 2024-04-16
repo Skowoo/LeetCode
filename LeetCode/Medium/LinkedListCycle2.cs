b03551812b08a56501838ceaadce4cb2c618a848
Skowoo <skowoo@tlen.pl>
2024-04-16 15:12:49 +0200

    LinkedListCycle2 done

---
+++ LeetCode/Medium/LinkedListCycle2.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal class LinkedListCycle2
+    {
+        public static ListNode? DetectCycle(ListNode head)
+        {
+            if (head is null || head.next is null)
+                return null;
+
+            ListNode slow = head, 
+                fast = head;
+
+            while (fast is not null && fast.next is not null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                    break;
+            }
+
+            if (fast is null || fast.next is null)
+                return null;
+
+            slow = head;
+
+            while (fast != slow)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow;
+        }
+
+
+        public static ListNode? DetectCycle_Naive(ListNode head)
+        {
+            if (head is null)
+                return null;
+
+            List<ListNode> visited = new();
+            ListNode temp = head;
+            while (temp.next is not null)
+                if (!visited.Contains(temp))
+                {
+                    visited.Add(temp);
+                    temp = temp.next;
+                }
+                else
+                    return temp;
+
+            return null;
+        }
+    }
+}

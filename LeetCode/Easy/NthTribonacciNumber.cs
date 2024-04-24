828b5144bc1bbde758c36cfe897bdba22dd2f437
Skowoo <skowoo@tlen.pl>
2024-04-24 08:01:12 +0200

    NthTribonacciNumber done

---
+++ LeetCode/Easy/NthTribonacciNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal class NthTribonacciNumber
+    {
+        public static int Tribonacci(int n)
+        {
+            int[] array = new int[3 + n];
+            array[0] = 0;
+            array[1] = 1;
+            array[2] = 1;
+
+            if (n <= 2)
+                return array[n];
+
+            for (int i = 3; i <= n; i++)
+                array[i] = array[i - 3] + array[i - 2] + array[i - 1];
+
+            return array[n];
+        }
+    }
+}

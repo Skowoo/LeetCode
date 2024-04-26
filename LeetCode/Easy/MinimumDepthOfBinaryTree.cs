4521ee29a4086d9e77a6a7a2e80e5b491920e392
Skowoo <skowoo@tlen.pl>
2024-04-26 08:00:41 +0200

    MinimumDepthOfBinaryTree done

---
+++ LeetCode/Easy/MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class MinimumDepthOfBinaryTree
+    {
+        public static int MinDepth(TreeNode root)
+        {
+            if (root is null)
+                return 0;
+
+            int leftResult = MinDepth(root.left),
+                rightResult = MinDepth(root.right);
+
+            if (leftResult == 0 || rightResult == 0)
+                return Math.Max(leftResult, rightResult);
+            else
+                return Math.Min(leftResult, rightResult);
+        }
+    }
+}

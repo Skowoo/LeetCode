d823465dce10844200438ea0505be63e54cc87d0
Skowoo <skowoo@tlen.pl>
2024-04-27 17:24:47 +0200

    DayOfTheWeek done

---
+++ LeetCode/Easy/DayOfTheWeek.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCode.Easy
+{
+    internal class DayOfTheWeek
+    {
+        public static string DayOfTheWeek_Func(int day, int month, int year) => new DateTime(year, month, day).DayOfWeek.ToString();
+    }
+}

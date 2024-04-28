77a8698f0a1849b584e3beacd1853f4756f13831
Skowoo <skowoo@tlen.pl>
2024-04-28 09:26:25 +0200

    SumOfDistancesInTree "done"

---
+++ LeetCode/Hard/SumOfDistancesInTree.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Hard
+{
+    internal class SumOfDistancesInTree
+    {
+        public static int[] SumOfDistancesInTree_Func(int n, int[][] edges)
+        {
+            var adjacencyMatrix = new List<int>[n]
+                .Select(x => x = new List<int>())
+                .ToArray();
+
+            int[] result = new int[n];
+            int[] auxCount = new int[n];
+
+            foreach (var e in edges)
+            {
+                adjacencyMatrix[e[0]].Add(e[1]);
+                adjacencyMatrix[e[1]].Add(e[0]);
+            }
+
+            Recurse(0, -1);
+            void Recurse(int node, int previousNode)
+            {
+                foreach (var c in adjacencyMatrix[node])
+                {
+                    if (c == previousNode)
+                        continue;
+
+                    Recurse(c, node);
+                    auxCount[node] += auxCount[c];
+                    result[node] += result[c] + auxCount[c];
+                }
+
+                auxCount[node]++;
+            }
+
+            RecurseAgain(0, -1);
+            void RecurseAgain(int node, int previousNode)
+            {
+                foreach (var c in adjacencyMatrix[node])
+                {
+                    if (c == previousNode)
+                        continue;
+
+                    result[c] = result[node] - auxCount[c] + n - auxCount[c];
+                    RecurseAgain(c, node);
+                }
+            }
+
+            return result;
+        }
+    }
+}

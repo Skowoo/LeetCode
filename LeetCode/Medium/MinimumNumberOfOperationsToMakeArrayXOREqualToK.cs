c27b21c4e62462201856a3cfbca85563ee80b47c
Skowoo <skowoo@tlen.pl>
2024-04-29 08:26:08 +0200

    MinimumNumberOfOperationsToMakeArrayXOREqualToK done

---
+++ LeetCode/Medium/MinimumNumberOfOperationsToMakeArrayXOREqualToK.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumNumberOfOperationsToMakeArrayXOREqualToK
+    {
+        public static int MinOperations(int[] nums, int k)
+        {
+            int result = 0,
+                initialXor = 0;
+
+            foreach (int i in nums)
+                initialXor ^= i;
+
+            if (initialXor != k)
+            {
+                int targetSum = initialXor ^ k;
+                while (targetSum > 0)
+                {
+                    result++;
+                    targetSum &= (targetSum - 1);
+                }
+                return result;
+            }
+            else
+                return result;
+        }
+    }
+}

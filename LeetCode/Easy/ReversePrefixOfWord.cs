38a3542408b69dcda6c2e5f0fd78634a98258df8
Skowoo <skowoo@tlen.pl>
2024-05-01 09:21:29 +0200

    ReversePrefixOfWord done

---
+++ LeetCode/Easy/ReversePrefixOfWord.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ReversePrefixOfWord
+    {
+        public static string ReversePrefix(string word, char ch)
+        {
+            var arr = word.ToCharArray();
+            int index = Array.IndexOf(arr, ch);
+            var reversed = arr.Take(index + 1).Reverse().ToArray();
+            string result = new(reversed);
+            result += new string(arr.Skip(index + 1).ToArray());
+            return result;
+        }
+    }
+}

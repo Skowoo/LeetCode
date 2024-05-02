ef61da1229fcc27480270e576dd3d60bdc3e26ab
Skowoo <skowoo@tlen.pl>
2024-05-02 09:18:04 +0200

    LargestPositiveIntegerThatExistsWithItsNegative done

---
+++ LeetCode/Easy/LargestPositiveIntegerThatExistsWithItsNegative.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy
+{
+    internal class LargestPositiveIntegerThatExistsWithItsNegative
+    {
+        public static int FindMaxK(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int left = 0,
+                right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (nums[left] < 0 && Math.Abs(nums[left]) == nums[right])
+                    return nums[right];
+                else if (Math.Abs(nums[left]) > nums[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return -1;
+        }
+    }
+}

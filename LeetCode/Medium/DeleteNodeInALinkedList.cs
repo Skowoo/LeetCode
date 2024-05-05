3b6c2a029f14cdbdf8befc9de814e4f3735e46a6
Skowoo <skowoo@tlen.pl>
2024-05-05 06:38:45 +0200

    DeleteNodeInALinkedList done

---
+++ LeetCode/Medium/DeleteNodeInALinkedList.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal class DeleteNodeInALinkedList
+    {
+        public static void DeleteNode(ListNode node)
+        {
+            ListNode currentNode = node,
+                previousNode = null;
+
+            while (currentNode.next is not null)
+            {
+                currentNode.val = currentNode.next.val;
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+
+            if (previousNode is not null)
+                previousNode.next = null;
+        }
+    }
+}

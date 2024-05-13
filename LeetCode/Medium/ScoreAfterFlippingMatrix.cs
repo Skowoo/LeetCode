da06ff6c30828362b9fde9f17f3ef5a2d06b1eb6
Skowoo <skowoo@tlen.pl>
2024-05-13 06:27:06 +0200

    ScoreAfterFlippingMatrix done

---
+++ LeetCode/Medium/ScoreAfterFlippingMatrix.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ScoreAfterFlippingMatrix
+    {
+        public static int MatrixScore(int[][] grid)
+        {
+            int score = grid.Length;
+            for (int c = 1; c < grid[0].Length; c++)
+            {
+                int count = 0;
+                for (int r = 0; r < grid.Length; r++)
+                {
+                    var row = grid[r];
+                    count += 1 ^ row[0] ^ row[c];
+                }
+                score = (score << 1) + Math.Max(count, grid.Length - count);
+            }
+            return score;
+        }
+    }
+}

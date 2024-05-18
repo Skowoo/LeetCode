a4e7f919765ed46bbefb96d66aa7ab5c67cbc355
Skowoo <skowoo@tlen.pl>
2024-05-18 09:20:42 +0200

    DistributeCoinsInBinaryTree done

---
+++ LeetCode/Medium/DistributeCoinsInBinaryTree.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal class DistributeCoinsInBinaryTree
+    {
+        public static int DistributeCoins(TreeNode root)
+        {
+            int result = 0;
+            int Recurse(TreeNode node)
+            {
+                if (node is null)
+                    return 0;
+
+                int currentDifference = node.val - 1 + Recurse(node.left) + Recurse(node.right);
+                result += Math.Abs(currentDifference);
+                return currentDifference;
+            }
+            Recurse(root);
+            return result;
+        }
+    }
+}

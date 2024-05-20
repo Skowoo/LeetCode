7d1f66d2b9bcd2dc5c7b159356a9f74ab1c95cd8
Skowoo <skowoo@tlen.pl>
2024-05-20 06:25:03 +0200

    SumOfAllSubsetXORTotals done

---
+++ LeetCode/Easy/SumOfAllSubsetXORTotals.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal class SumOfAllSubsetXORTotals
+    {
+        public static int SubsetXORSum(int[] nums)
+        {
+            int Recurse(int index, int result)
+            {
+                if (index == nums.Length)
+                    return result;
+                else
+                    return Recurse(index + 1, result ^ nums[index]) + Recurse(index + 1, result);
+            }
+            return Recurse(0, 0);
+        }
+    }
+}

58ff82bff2619e17017fd81d6eae93e11ef6be77
Skowoo <skowoo@tlen.pl>
2024-05-21 06:28:00 +0200

    Subsets done

---
+++ LeetCode/Medium/Subsets.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Medium
+{
+    internal class Subsets
+    {
+        public static IList<IList<int>> Subsets_Func(int[] nums)
+        {
+            List<List<int>> result =
+            [
+                []
+            ];
+
+            foreach (int n in nums)
+            {
+                int currSize = result.Count;
+                for (int i = 0; i < currSize; i++)
+                {
+                    List<int> subset = new(result[i]) { n };
+                    result.Add(subset);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}

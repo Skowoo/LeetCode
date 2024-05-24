b2b44bb248f07e094010474de9f72f8bdc37448f
Skowoo <skowoo@tlen.pl>
2024-05-24 08:36:55 +0200

    MaximumScoreWordsFormedByLetters simplified

--- LeetCode/Hard/MaximumScoreWordsFormedByLetters.cs
+++ LeetCode/Hard/MaximumScoreWordsFormedByLetters.cs
@@ -2,7 +2,67 @@
 {
     internal class MaximumScoreWordsFormedByLetters
     {
+        // Simplified, yet still not perfect code
         public static int MaxScoreWords(string[] words, char[] letters, int[] score)
+        {
+            int finalResult = 0;
+            Dictionary<char, int> lettersDictionary = [];
+            foreach (char c in letters)
+                if (lettersDictionary.TryGetValue(c, out int value))
+                    lettersDictionary[c]++;
+                else
+                    lettersDictionary[c] = 1;
+
+            string[] validWords = words.Where(x => CheckIfWordsAreValid([x])).ToArray();
+            FindAndCheckSubsets(validWords);
+
+            return finalResult;
+
+            int CalculateWordsScore(IEnumerable<string> words)
+            {
+                int result = 0;
+                foreach (var word in words)
+                    foreach (char c in word)
+                        result += score[c - 'a'];
+
+                return result;
+            }
+
+            bool CheckIfWordsAreValid(IEnumerable<string> words)
+            {
+                var copy = new Dictionary<char, int>(lettersDictionary);
+
+                foreach (var word in words)
+                    foreach (char c in word)
+                        if (copy.TryGetValue(c, out int count))
+                            if (count > 1)
+                                copy[c]--;
+                            else
+                                copy.Remove(c);
+                        else
+                            return false;
+
+                return true;
+            }
+
+            void FindAndCheckSubsets(string[] input)
+            {
+                int n = input.Length;
+                for (int i = 0; i < (1 << n); i++)
+                {
+                    List<string> subset = [];
+                    for (int j = 0; j < n; j++)
+                        if ((i & (1 << j)) != 0)
+                            subset.Add(input[j]);
+
+                    if (CheckIfWordsAreValid(subset))
+                        finalResult = Math.Max(finalResult, CalculateWordsScore(subset));
+                }
+            }
+        }
+
+
+        public static int MaxScoreWords__Old(string[] words, char[] letters, int[] score)
         {
             int finalResult = 0;
             Dictionary<char, int> lettersDictionary = [];

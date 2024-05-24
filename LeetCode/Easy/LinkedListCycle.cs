569fde909536bc68a19c61cde445362681d570d0
Skowoo <skowoo@tlen.pl>
2024-05-24 05:58:04 +0200

    LinkedListCycle improved

--- LeetCode/Easy/LinkedListCycle.cs
+++ LeetCode/Easy/LinkedListCycle.cs
@@ -4,6 +4,26 @@ namespace LeetCode.Easy
 {
     internal class LinkedListCycle
     {
+        // Rabbit & Hare solution
+        public static bool HasCycle(ListNode head)
+        {
+            if (head is null || head.next is null)
+                return false;
+
+            ListNode slow = head,
+                     fast = head;
+
+            while (fast.next is not null && fast.next.next is not null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                    return true;
+            }
+            return false;
+        }
+
         // Extremely uneffective solution - beats 5% :<
         public static bool HasCycleBAD(ListNode head)
         {

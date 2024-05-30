bda6ec804c950728f8cd52e92be77d3394c2702e
Skowoo <skowoo@tlen.pl>
2024-05-30 08:58:21 +0200

    CountTripletsThatCanFormTwoArraysOfEqualXOR done

---
+++ LeetCode/Medium/CountTripletsThatCanFormTwoArraysOfEqualXOR.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Medium
+{
+    internal class CountTripletsThatCanFormTwoArraysOfEqualXOR
+    {
+        public static int CountTriplets(int[] arr)
+        {
+            int[] cummulativeResult = new int[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++)
+                cummulativeResult[i + 1] = cummulativeResult[i] ^ arr[i];
+
+            int result = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (cummulativeResult[i] == cummulativeResult[j + 1])
+                        result += (j - i);
+
+            return result;
+        }
+    }
+}

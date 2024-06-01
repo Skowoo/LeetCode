3eea99595d6f08190406d3ac672bbef6e37b3080
Skowoo <skowoo@tlen.pl>
2024-06-01 07:06:25 +0200

    ScoreOfAString done

---
+++ LeetCode/Easy/ScoreOfAString.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ScoreOfAString
+    {
+        public static int ScoreOfString(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length - 1; i++)
+                result += Math.Abs(s[i] - s[i + 1]);
+
+            return result;
+        }
+    }
+}

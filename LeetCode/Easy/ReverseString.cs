9e408825de67988c351a9fbc05f3cc8627652e12
Skowoo <skowoo@tlen.pl>
2024-06-02 07:38:04 +0200

    ReverseString done

---
+++ LeetCode/Easy/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ReverseString
+    {
+        public static void ReverseString_Func(char[] s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+                (s[i], s[s.Length - i - 1]) = (s[i], s[s.Length - i - 1]);
+        }
+    }
+}

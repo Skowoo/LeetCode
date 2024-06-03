f28b7cc2643b87ea1a83028f63f582edae699250
Skowoo <skowoo@tlen.pl>
2024-06-03 06:20:51 +0200

    AppendCharactersToStringToMakeSubsequence done

---
+++ LeetCode/Medium/AppendCharactersToStringToMakeSubsequence.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Medium
+{
+    internal class AppendCharactersToStringToMakeSubsequence
+    {
+        public static int AppendCharacters(string s, string t)
+        {
+            int pointerT = 0,
+                pointerS = 0;
+
+            while (pointerS < s.Length && pointerT < t.Length)
+            {
+                if (t[pointerT] == s[pointerS])
+                    pointerT++;
+                pointerS++;
+            }
+
+            return t.Length - pointerT;
+        }
+    }
+}

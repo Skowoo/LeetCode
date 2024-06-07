309377a41f5dd5de1ad8dab3263b181f59f99c8e
Skowoo <skowoo@tlen.pl>
2024-06-07 06:08:21 +0200

    ReplaceWords done

---
+++ LeetCode/Medium/ReplaceWords.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ReplaceWords
+    {
+        // Uneffective solution
+        public static string ReplaceWords_Uneffective(IList<string> dictionary, string sentence)
+        {
+            IList<string> words = sentence.Split(' ');
+            for (int i = 0; i < words.Count; i++)
+            {
+                var candidate = dictionary
+                    .Where(x => words[i]
+                    .StartsWith(x))
+                    .OrderBy(x => x.Length)
+                    .FirstOrDefault();
+
+                if (!string.IsNullOrEmpty(candidate))
+                    words[i] = candidate;
+            }
+            return string.Join(" ", words);
+        }
+    }
+}

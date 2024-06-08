bef2d98e36944c1ca348142660577b030cd63c42
Skowoo <skowoo@tlen.pl>
2024-06-08 07:18:52 +0200

    ContinuousSubarraySum done

---
+++ LeetCode/Medium/ContinuousSubarraySum.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium
+{
+    internal class ContinuousSubarraySum
+    {
+        public static bool CheckSubarraySum(int[] nums, int k)
+        {
+            int previousModulo = nums[0] % k,
+                currentModulo;
+            HashSet<int> moduloSet = [];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentModulo = (previousModulo + nums[i]) % k;
+
+                if (currentModulo % k == 0 || moduloSet.Contains(currentModulo))
+                    return true;
+
+                moduloSet.Add(previousModulo);
+                previousModulo = currentModulo;
+            }
+            return false;
+        }
+    }
+}

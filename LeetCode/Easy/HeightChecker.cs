b70cc880cfac31f290dd1e440172ac3dcb22beaf
Skowoo <skowoo@tlen.pl>
2024-06-10 05:19:19 +0200

    HeightChecker done

---
+++ LeetCode/Easy/HeightChecker.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    internal class HeightChecker
+    {
+        public int HeightChecker_Fnc(int[] heights)
+        {
+            int[] expected = new int[heights.Length];
+            Array.Copy(heights, expected, heights.Length);
+            Array.Sort(expected);
+            int result = 0;
+            for (int i = 0; i < heights.Length; i++)
+                if (heights[i] != expected[i])
+                    result++;
+
+            return result;
+        }
+    }
+}

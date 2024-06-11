89cab4e4b06c468079476726d1b2ea80d31e4fa2
Skowoo <skowoo@tlen.pl>
2024-06-11 05:30:26 +0200

    RelativeSortArray done

---
+++ LeetCode/Easy/RelativeSortArray.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy
+{
+    internal class RelativeSortArray
+    {
+        public static int[] RelativeSortArray_Fnc(int[] arr1, int[] arr2)
+        {
+            Dictionary<int, int> ordered = [];
+            List<int> leftover = [];
+
+            foreach (int num in arr1)
+                if (arr2.Contains(num))
+                    if (ordered.ContainsKey(num))
+                        ordered[num]++;
+                    else
+                        ordered[num] = 1;
+                else
+                    leftover.Add(num);
+
+            List<int> result = [];
+
+            foreach (int num in arr2)
+                for (int i = 0; i < ordered[num]; i++)
+                    result.Add(num);
+
+            result.AddRange(leftover.OrderBy(x => x));
+
+            return [.. result];
+        }
+    }
+}

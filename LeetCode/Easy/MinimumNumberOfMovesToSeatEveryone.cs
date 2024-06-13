e32e4572a3cca71eb3878d2f3823abe176dc2447
Skowoo <skowoo@tlen.pl>
2024-06-13 06:00:25 +0200

    MinimumNumberOfMovesToSeatEveryone done

---
+++ LeetCode/Easy/MinimumNumberOfMovesToSeatEveryone.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MinimumNumberOfMovesToSeatEveryone
+    {
+        public static int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+
+            int result = 0;
+
+            for (int i = 0; i < seats.Length; i++)
+                result += Math.Abs(seats[i] - students[i]);
+
+            return result;
+        }
+    }
+}

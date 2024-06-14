9f47c31a754c65e22e27c36514f48c914ea8fdc8
Skowoo <skowoo@tlen.pl>
2024-06-14 06:15:14 +0200

    MinimumIncrementToMakeArrayUnique done

---
+++ LeetCode/Medium/MinimumIncrementToMakeArrayUnique.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumIncrementToMakeArrayUnique
+    {
+        public static int MinIncrementForUnique(int[] nums)
+        {
+            int result = 0;
+
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i++)
+                if (nums[i] <= nums[i - 1])
+                {
+                    int value = nums[i - 1] - nums[i] + 1;
+                    nums[i] += value;
+                    result += value;
+                }
+
+            return result;
+        }
+    }
+}

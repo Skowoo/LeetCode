5fe2768f010f9701d568e39d431cffffb0f10cf4
Skowoo <skowoo@tlen.pl>
2024-06-15 07:17:26 +0200

    IPO done

---
+++ LeetCode/Hard/IPO.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Hard
+{
+    internal class IPO
+    {
+        public static int FindMaximizedCapital(int k, int w, int[] profits, int[] capital)
+        {
+            Array.Sort(capital, profits);
+            PriorityQueue<int, int> queue = new();
+            int indexer = 0;
+            while (k > 0)
+            {
+                while (indexer < capital.Length && w >= capital[indexer])
+                {
+                    queue.Enqueue(profits[indexer], -profits[indexer]);
+                    indexer++;
+                }
+                if (queue.Count == 0)
+                    return w;
+                w += queue.Dequeue();
+                k--;
+            }
+            return w;
+        }
+    }
+}

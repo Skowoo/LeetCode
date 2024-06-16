77a1711836a88e889505ef5a27277a3436cc1493
Skowoo <skowoo@tlen.pl>
2024-06-16 08:27:35 +0200

    PatchingArray done

---
+++ LeetCode/Hard/PatchingArray.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Hard
+{
+    internal class PatchingArray
+    {
+        public static int MinPatches(int[] nums, int n)
+        {
+            int result = 0,
+                indexer = 0;
+            long target = 1;
+
+            while (target <= n)
+            {
+                if (indexer < nums.Length && nums[indexer] <= target)
+                    target += nums[indexer++];
+                else
+                {
+                    target += target;
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}

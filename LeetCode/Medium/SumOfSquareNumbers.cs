bb2f354fca77a855a39909a882a418803df1b46f
Skowoo <skowoo@tlen.pl>
2024-06-17 06:23:32 +0200

    SumOfSquareNumbers done

---
+++ LeetCode/Medium/SumOfSquareNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium
+{
+    internal class SumOfSquareNumbers
+    {
+        public static bool JudgeSquareSum(int c)
+        {
+            int a = 0;
+            int b = (int)Math.Ceiling(Math.Sqrt(c));
+
+            while (a <= b)
+            {
+                int candidate = a * a + b * b;
+                if (candidate == c)
+                    return true;
+                else if (candidate < c && 0 <= candidate)
+                    a++;
+                else
+                    b--;
+            }
+
+            return false;
+        }
+    }
+}

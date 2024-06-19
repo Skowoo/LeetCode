55bc4fc2bfc926c72d6109cb8ec6e3fd7e9a3387
Skowoo <skowoo@tlen.pl>
2024-06-19 07:27:54 +0200

    MinimumNumberOfDaysToMakeMBouquets done

---
+++ LeetCode/Medium/MinimumNumberOfDaysToMakeMBouquets.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumNumberOfDaysToMakeMBouquets
+    {
+
+        public static int MinDays(int[] bloomDay, int m, int k)
+        {
+            if ((long)(m * k) > bloomDay.Length)
+                return -1;
+
+            int result = -1,
+                start = 0,
+                end = 0;
+
+            foreach (int val in bloomDay)
+                end = Math.Max(end, val);
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (Count(mid) >= m)
+                {
+                    result = mid;
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return result;
+
+            int Count(int value)
+            {
+                int result = 0,
+                    flowers = 0;
+
+                foreach (var day in bloomDay)
+                    if (day <= value)
+                    {
+                        flowers++;
+                        if (flowers == k)
+                        {
+                            result++;
+                            flowers = 0;
+                        }
+                    }
+                    else
+                    {
+                        flowers = 0;
+                    }
+
+                return result;
+            }
+        }
+    }
+}

d7c06cc30746d29ee213774b4d20c82adab3d65c
Skowoo <skowoo@tlen.pl>
2024-06-20 08:53:32 +0200

    MagneticForceBetweenTwoBalls done

---
+++ LeetCode/Medium/MagneticForceBetweenTwoBalls.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MagneticForceBetweenTwoBalls
+    {
+        public static int MaxDistance(int[] position, int m)
+        {
+            Array.Sort(position);
+
+            int result = 0,
+                low = 1,
+                high = position[^1] - position[0];
+
+            while (low <= high)
+            {
+                int middle = low + (high - low) / 2;
+                if (Check(middle))
+                {
+                    result = middle;
+                    low = middle + 1;
+                }
+                else
+                {
+                    high = middle - 1;
+                }
+            }
+
+            return result;
+
+            bool Check(int candidate)
+            {
+                int count = 1;
+                int prevPos = position[0];
+
+                for (int i = 1; i < position.Length; i++)
+                    if (position[i] - prevPos >= candidate)
+                    {
+                        prevPos = position[i];
+                        count++;
+
+                        if (count == m)
+                            return true;
+                    }
+
+                return false;
+            }
+        }
+    }
+}

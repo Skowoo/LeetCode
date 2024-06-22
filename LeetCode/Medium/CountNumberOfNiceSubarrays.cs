a6e6bcc022225acb2524a9f98fa4d847150a5809
Skowoo <skowoo@tlen.pl>
2024-06-22 08:16:00 +0200

    CountNumberOfNiceSubarrays done

---
+++ LeetCode/Medium/CountNumberOfNiceSubarrays.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium
+{
+    internal class CountNumberOfNiceSubarrays
+    {
+        public static int NumberOfSubarrays(int[] nums, int k)
+        {
+            int currSum = 0,
+                result = 0;
+
+            Dictionary<int, int> prefixDictionary = new(nums.Length) { [0] = 1 };
+
+            foreach (int i in nums)
+            {
+                currSum += i & 1;
+
+                if (prefixDictionary.ContainsKey(currSum - k))
+                    result += prefixDictionary[currSum - k];
+
+                prefixDictionary[currSum] = prefixDictionary.GetValueOrDefault(currSum, 0) + 1;
+            }
+            return result;
+        }
+    }
+}

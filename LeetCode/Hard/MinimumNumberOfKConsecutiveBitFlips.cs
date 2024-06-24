055b52eae212779e9bdb19d4aaa382c49f19c9e3
Skowoo <skowoo@tlen.pl>
2024-06-24 08:16:41 +0200

    MinimumNumberOfKConsecutiveBitFlips done

---
+++ LeetCode/Hard/MinimumNumberOfKConsecutiveBitFlips.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Hard
+{
+    internal class MinimumNumberOfKConsecutiveBitFlips
+    {
+        public static int MinKBitFlips(int[] nums, int k)
+        {
+            int result = 0,
+                switchCounter = 0;
+            int[] switched = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i >= k)
+                    switchCounter ^= switched[i - k];
+
+                if (nums[i] == switchCounter % 2)
+                {
+                    if (i + k > nums.Length)
+                        return -1;
+
+                    switched[i] = 1;
+                    switchCounter ^= 1;
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}

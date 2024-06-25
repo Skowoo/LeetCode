bf5845a7295a62de23ce3d06884ff55e74ac6ffb
Skowoo <skowoo@tlen.pl>
2024-06-25 08:25:54 +0200

    BinarySearchTreeToGreaterSumTree done

---
+++ LeetCode/Medium/BinarySearchTreeToGreaterSumTree.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    // Valid also for LeetCode 538. Convert BST to Greater Tree.
+    internal class BinarySearchTreeToGreaterSumTree
+    {
+        public static TreeNode BstToGst(TreeNode root)
+        {
+            int totalSum = 0;
+
+            Recurse(root);
+
+            return root;
+
+            void Recurse(TreeNode node)
+            {
+                if (node is null)
+                    return;
+
+                Recurse(node.right);
+
+                totalSum += node.val;
+                node.val = totalSum;
+
+                Recurse(node.left);
+            }
+        }
+    }
+}

2ade7c52ce639de417cec253d13e7b7ca3dee1c8
Skowoo <skowoo@tlen.pl>
2024-06-27 07:28:11 +0200

    FindCenterOfStarGraph done

---
+++ LeetCode/Easy/FindCenterOfStarGraph.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal class FindCenterOfStarGraph
+    {
+        public static int FindCenter(int[][] edges)
+        {
+            if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+                return edges[0][0];
+            else
+                return edges[0][1];
+        }
+
+        // LINQ solution
+        public static int FindCenter_LINQ(int[][] edges) => edges[0].Intersect(edges[1]).Single();
+    }
+}

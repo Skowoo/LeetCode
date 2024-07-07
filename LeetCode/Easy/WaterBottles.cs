ae1a8c3883b4da7aeff46b0129e3e5de50a31614
Skowoo <skowoo@tlen.pl>
2024-07-07 08:24:18 +0200

    WaterBottles done

---
+++ LeetCode/Easy/WaterBottles.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal class WaterBottles
+    {
+        public static int NumWaterBottles(int numBottles, int numExchange)
+        {
+            int emptyBottles = 0,
+                result = 0;
+
+            do
+            {
+                result += numBottles;
+                emptyBottles += numBottles;
+                numBottles = emptyBottles / numExchange;
+                emptyBottles = emptyBottles % numExchange;
+            }
+            while (numBottles > 0);
+
+            return result;
+        }
+    }
+}

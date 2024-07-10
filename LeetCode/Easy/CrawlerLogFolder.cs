c521ab690bab253193b732c81ccba612df3d8b95
Skowoo <skowoo@tlen.pl>
2024-07-10 05:59:32 +0200

    CrawlerLogFolder done

---
+++ LeetCode/Easy/CrawlerLogFolder.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy
+{
+    internal class CrawlerLogFolder
+    {
+        public static int MinOperations(string[] logs)
+        {
+            int result = 0;
+            foreach (var log in logs)
+            {
+                switch (log)
+                {
+                    case "../":
+                        if (result > 0)
+                            result--;
+                        break;
+                    case "./":
+                        break;
+                    default:
+                        result++;
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}

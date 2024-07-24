150cc352ba98066ea62f32df6feb1461f39e63cc
Skowoo <skowoo@tlen.pl>
2024-07-24 06:17:46 +0200

    SortTheJumbledNumbers done

---
+++ LeetCode/Medium/SortTheJumbledNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCode.Medium
+{
+    internal class SortTheJumbledNumbers
+    {
+        public static int[] SortJumbled(int[] mapping, int[] nums)
+        {
+            int[] comparer = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                string str = nums[i].ToString();
+
+                StringBuilder sb = new();
+                for (int j = 0; j < str.Length; j++)
+                    sb.Append(mapping[str[j] - '0']);
+
+                comparer[i] = int.Parse(sb.ToString());
+            }
+
+            Array.Sort(comparer, nums);
+
+            return nums;
+        }
+    }
+}
+
+// Console.WriteLine(SortJumbled([8, 9, 4, 0, 2, 1, 3, 5, 7, 6], [991, 338, 38]));

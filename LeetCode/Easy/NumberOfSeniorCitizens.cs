8d8c11d3538ca84ba5e7a426b0091f9e268c230d
Skowoo <skowoo@tlen.pl>
2024-08-01 06:12:11 +0200

    NumberOfSeniorCitizens done

---
+++ LeetCode/Easy/NumberOfSeniorCitizens.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    internal class NumberOfSeniorCitizens
+    {
+        public static int CountSeniors(string[] details)
+        {
+            int result = 0;
+            foreach (string s in details)
+                if (s[11] == '6' && s[12] > '0' || s[11] > '6')
+                    result++;
+
+            return result;
+        }
+    }
+}

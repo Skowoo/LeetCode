0d77f609f986e42afb2696d9f15f10141e7787c7
Skowoo <skowoo@tlen.pl>
2024-08-05 05:49:36 +0200

    KthDistinctStringInAnArray done

---
+++ LeetCode/Easy/KthDistinctStringInAnArray.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    internal class KthDistinctStringInAnArray
+    {
+        public static string KthDistinct(string[] arr, int k)
+        {
+            Dictionary<string, int> dict = [];
+            foreach (var item in arr)
+                if (dict.TryGetValue(item, out int value))
+                    dict[item] = ++value;
+                else
+                    dict.Add(item, 1);
+
+            var distinct = dict.Where(x => x.Value == 1).Select(x => x.Key).ToList();
+            return k > distinct.Count ? "" : distinct[k - 1];
+        }
+    }
+}

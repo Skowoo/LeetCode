3e9ee0d97699b33744fac8440a9f1844a62c1e0c
Skowoo <skowoo@tlen.pl>
2024-08-06 05:36:52 +0200

    MinimumNumberOfPushesToTypeWordII done

---
+++ LeetCode/Medium/MinimumNumberOfPushesToTypeWordII.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumNumberOfPushesToTypeWordII
+    {
+        public static int MinimumPushes(string word)
+        {
+            Dictionary<char, int> letters = [];
+            foreach (char c in word)
+                if (letters.TryGetValue(c, out int value))
+                    letters[c] = ++value;
+                else
+                    letters.Add(c, 1);
+
+            int result = 0,
+                index = 0;
+
+            foreach (var letter in letters.OrderByDescending(x => x.Value))
+            {
+                result += letter.Value * (1 + (index / 8));
+                index++;
+            }
+
+            return result;
+        }
+    }
+}

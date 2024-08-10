def4b309c68015036ee076c56991953e724b4e1c
Skowoo <skowoo@tlen.pl>
2024-08-10 21:05:12 +0200

    Evening coding...

---
+++ LeetCode/Easy/PowerOfThree.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy
+{
+    internal class PowerOfThree
+    {
+        public static bool IsPowerOfThree(int n)
+        {
+            int[] powers = [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467];
+            return powers.Contains(n);
+        }
+    }
+}
---
+++ LeetCode/Medium/RectangleArea.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Medium
+{
+    internal class RectangleArea
+    {
+        public static int ComputeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+        {
+            int areaA = ((ax2 - ax1) * (ay2 - ay1)),
+                areaB = ((bx2 - bx1) * (by2 - by1));
+
+            if (bx1 >= ax2 || ax1 >= bx2 || by1 >= ay2 || ay1 >= by2)
+                return areaA + areaB;
+
+            int overlapArea = (Math.Min(ax2, bx2) - Math.Max(ax1, bx1)) * (Math.Min(ay2, by2) - Math.Max(ay1, by1));
+            return areaA + areaB - overlapArea;
+        }
+    }
+}

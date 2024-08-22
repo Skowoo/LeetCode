1aa68210c1531f8fb0fe211b35440029d670045d
Skowoo <skowoo@tlen.pl>
2024-08-22 05:48:12 +0200

    NumberComplement done

---
+++ LeetCode/Easy/NumberComplement.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Easy
+{
+    internal class NumberComplement // + 1009 Complement of Base 10 Integer
+    {
+        public static int FindComplement(int num)
+        {
+            int mask = 1;
+            while (mask < num)
+                mask = (mask << 1) | 1;
+            return num ^ mask;
+        }
+    }
+}

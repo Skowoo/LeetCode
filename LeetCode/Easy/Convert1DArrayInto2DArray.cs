a61e857e9bc57c737c18531c775de0bdc85bdad4
Skowoo <skowoo@tlen.pl>
2024-09-01 07:59:12 +0200

    Convert1DArrayInto2DArray done

---
+++ LeetCode/Easy/Convert1DArrayInto2DArray.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    class Convert1DArrayInto2DArray
+    {
+        public static int[][] Construct2DArray(int[] original, int m, int n)
+        {
+            if (m * n != original.Length)
+                return [];
+
+            int[][] result = new int[m][];
+            for (int i = 0; i < m; i++)
+            {
+                result[i] = new int[n];
+                for (int j = 0; j < n; j++)
+                    result[i][j] = original[i * n + j];
+            }
+
+            return result;
+        }
+    }
+}
+
+// Console.WriteLine(Construct2DArray([1,2,3,4,5,6,7,8,9,10,11,12], 4, 3));

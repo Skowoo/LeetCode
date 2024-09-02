8d27de49a33b9049d4b71845c1aa45c9ec313baf
Skowoo <skowoo@tlen.pl>
2024-09-02 08:15:59 +0200

    FindTheStudentThatWillReplaceTheChalk done

---
+++ LeetCode/Medium/FindTheStudentThatWillReplaceTheChalk.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Medium
+{
+    internal class FindTheStudentThatWillReplaceTheChalk
+    {
+        public static int ChalkReplacer(int[] chalk, int k)
+        {
+            long sum = 0;
+            foreach (var c in chalk)
+                sum += c;
+
+            long leftover = k % sum;
+            int indexer = 0;
+
+            while (leftover >= 0)
+                leftover -= chalk[indexer++];
+
+            return indexer - 1;
+        }
+    }
+}

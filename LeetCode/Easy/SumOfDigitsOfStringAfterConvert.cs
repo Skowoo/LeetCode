c6522a636a40990bfcccea090d3167777988ffea
Skowoo <skowoo@tlen.pl>
2024-09-03 15:55:40 +0200

    SumOfDigitsOfStringAfterConvert done

---
+++ LeetCode/Easy/SumOfDigitsOfStringAfterConvert.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal class SumOfDigitsOfStringAfterConvert
+    {
+        public static int GetLucky(string s, int k)
+        {
+            StringBuilder baseSb = new();
+            foreach (char c in s)
+                baseSb.Append(c - 96);
+
+            string numberString = baseSb.ToString();
+            int result = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                result = numberString.Sum(c => c - 48);
+                numberString = result.ToString();
+            }
+
+            return result;
+        }
+    }
+}

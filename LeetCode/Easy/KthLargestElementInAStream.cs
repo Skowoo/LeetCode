bbe44647e78f0fe3ad3c81527b66e07020fbb70f
Skowoo <skowoo@tlen.pl>
2024-09-04 18:37:23 +0200

    KthLargestElementInAStream done

---
+++ LeetCode/Easy/KthLargestElementInAStream.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy
+{
+    public class KthLargest
+    {
+        PriorityQueue<int, int> pq = new();
+        int k;
+
+        public KthLargest(int k, int[] nums)
+        {
+            this.k = k;
+
+            foreach (var num in nums)
+                Add(num);
+        }
+
+        public int Add(int val)
+        {
+            pq.Enqueue(val, val);
+
+            while (pq.Count > k)
+                pq.Dequeue();
+
+            return pq.Peek();
+        }
+    }
+}

fd6a2d701ebb265b4c7e24e1c547b912a4c5d4f4
Skowoo <skowoo@tlen.pl>
2024-09-05 20:00:03 +0200

    PathSum done

---
+++ LeetCode/Easy/PathSum.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class PathSum
+    {
+        public static bool HasPathSum(TreeNode root, int targetSum)
+        {
+            return Recurse(root, 0);
+
+            bool Recurse(TreeNode root, int currSum)
+            {
+                if (root is null)
+                    return false;
+
+                currSum += root.val;
+
+                if (root.left is null && root.right is null)
+                    return currSum == targetSum;
+
+                if (Recurse(root.left!, currSum))
+                    return true;
+
+                if (Recurse(root.right, currSum))
+                    return true;
+
+                return false;
+            }
+        }
+    }
+}

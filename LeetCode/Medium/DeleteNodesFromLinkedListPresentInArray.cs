428f77e318ca462504f02278fe2814198ca63b18
Skowoo <skowoo@tlen.pl>
2024-09-06 08:37:24 +0200

    DeleteNodesFromLinkedListPresentInArray done

---
+++ LeetCode/Medium/DeleteNodesFromLinkedListPresentInArray.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal class DeleteNodesFromLinkedListPresentInArray
+    {
+        public static ListNode ModifiedList(int[] nums, ListNode head)
+        {
+            HashSet<int> numsSet = [.. nums];
+            ListNode previousNode = null!;
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (!numsSet.Contains(currentNode.val))
+                    (previousNode, currentNode) = (currentNode, currentNode.next);
+                else if (previousNode is null)
+                    (head, currentNode) = (currentNode.next, currentNode.next);
+                else if (currentNode.next is null)
+                    (currentNode, previousNode.next) = (currentNode.next!, null!);
+                else
+                    (previousNode.next, currentNode) = (currentNode.next, currentNode.next);
+            }
+
+            return head;
+        }
+    }
+}

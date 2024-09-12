35d27099cdfbd13585426cf94214840cfaff8c86
Skowoo <skowoo@tlen.pl>
2024-09-12 06:34:26 +0200

    CountTheNumberOfConsistentStrings done

---
+++ LeetCode/Easy/CountTheNumberOfConsistentStrings.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    internal class CountTheNumberOfConsistentStrings
+    {
+        public static int CountConsistentStrings(string allowed, string[] words)
+        {
+            var allowedArray = allowed.ToCharArray();
+            int result = 0;
+            foreach (string word in words)
+            {
+                bool valid = true;
+                foreach (char c in word)
+                    if (!allowedArray.Contains(c))
+                    {
+                        valid = false;
+                        break;
+                    }
+                if (valid)
+                    result++;
+            }
+            return result;
+        }
+    }
+}

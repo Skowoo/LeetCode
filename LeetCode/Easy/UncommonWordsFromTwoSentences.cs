5cd1e860dec97c7bb4576794ee138bece75fe67c
Skowoo <skowoo@tlen.pl>
2024-09-17 06:18:37 +0200

    UncommonWordsFromTwoSentences done

---
+++ LeetCode/Easy/UncommonWordsFromTwoSentences.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    internal class UncommonWordsFromTwoSentences
+    {
+        public static string[] UncommonFromSentences(string s1, string s2)
+        {
+            Dictionary<string, int> wordsCount = [];
+            foreach (string s in s1.Split(' '))
+                if (wordsCount.TryGetValue(s, out int value))
+                    wordsCount[s] = ++value;
+                else
+                    wordsCount.Add(s, 1);
+
+            foreach (string s in s2.Split(' '))
+                if (wordsCount.TryGetValue(s, out int value))
+                    wordsCount[s] = ++value;
+                else
+                    wordsCount.Add(s, 1);
+
+            return wordsCount.Where(x => x.Value == 1).Select(x => x.Key).ToArray();
+        }
+    }
+}

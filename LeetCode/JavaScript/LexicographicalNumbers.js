d304c1132088de51aa619255ab4da55992e7523c
Skowoo <skowoo@tlen.pl>
2024-09-21 07:17:04 +0200

    JavaScipt solution catalog renamed LexicographicalNumbers done in C# and JS.

---
+++ LeetCode/JavaScript/LexicographicalNumbers.js
@@ -0,0 +1,11 @@
+﻿/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var lexicalOrder = function (n) {
+    return Array
+        .from({ length: n }, (_, i) => (i + 1)
+            .toString())
+        .sort()
+        .map(Number)
+};
---
+++ LeetCode/Medium/LexicographicalNumbers.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Medium
+{
+    internal class LexicographicalNumbers
+    {
+        public static IList<int> LexicalOrder(int n)
+        {
+            List<int> result = Enumerable.Range(1, n).ToList();
+            return result.OrderBy(x => x.ToString()).Select(x => (int)x).ToList();
+        }
+    }
+}

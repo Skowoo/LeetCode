22a9beca4b4f1a90fcf4a2b1f7f2971febc5a83d
Skowoo <skowoo@tlen.pl>
2024-10-02 18:52:58 +0200

    RankTransformOfAnArray done

---
+++ LeetCode/Easy/RankTransformOfAnArray.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal class RankTransformOfAnArray
+    {
+        public static int[] ArrayRankTransform(int[] arr)
+        {
+            Dictionary<int, int> dic = [];
+            foreach (int i in arr)
+                dic.TryAdd(i, i);
+
+            int rank = 1;
+            foreach (var i in dic.OrderBy(x => x.Key))
+                dic[i.Key] = rank++;
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = dic[arr[i]];
+
+            return arr;
+        }
+    }
+}

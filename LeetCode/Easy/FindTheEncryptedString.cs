2027f336cd6eb7ce37a1236383c6501a703866a5
Skowoo <skowoo@tlen.pl>
2024-10-05 10:36:24 +0200

    FindTheEncryptedString done

---
+++ LeetCode/Easy/FindTheEncryptedString.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal class FindTheEncryptedString
+    {
+        public static string GetEncryptedString(string s, int k)
+        {
+            StringBuilder sb = new(s);
+            for (int i = 0; i < s.Length; i++)
+                sb[i] = (char)s[(i + k) % s.Length];
+
+            return sb.ToString();
+        }
+    }
+}

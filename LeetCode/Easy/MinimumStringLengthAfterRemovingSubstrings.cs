94d19358e5077091c094a8a076f18349f9e711c1
Skowoo <skowoo@tlen.pl>
2024-10-07 15:34:36 +0200

    MinimumStringLengthAfterRemovingSubstrings done

---
+++ LeetCode/Easy/MinimumStringLengthAfterRemovingSubstrings.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal class MinimumStringLengthAfterRemovingSubstrings
+    {
+        public static int MinLength(string s)
+        {
+            StringBuilder sb;
+            while (s.Contains("AB") || s.Contains("CD"))
+            {
+                sb = new(s);
+                sb.Replace("AB", null);
+                sb.Replace("CD", null);
+                s = sb.ToString();
+            }
+            return s.Length;
+        }
+    }
+}

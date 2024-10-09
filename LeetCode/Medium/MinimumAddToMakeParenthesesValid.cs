0d811040764eb69114f90a8f52ea05d0e455e32c
Skowoo <skowoo@tlen.pl>
2024-10-09 10:30:12 +0200

    MinimumAddToMakeParenthesesValid done

---
+++ LeetCode/Medium/MinimumAddToMakeParenthesesValid.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumAddToMakeParenthesesValid
+    {
+        public static int MinAddToMakeValid(string s)
+        {
+            int left = 0,
+                right = 0;
+
+            foreach (char c in s)
+            {
+                if (c == '(')
+                    left++;
+                else if (left <= 0)
+                    right++;
+                else
+                    left--;
+            }
+
+            return left + right;
+        }
+    }
+}

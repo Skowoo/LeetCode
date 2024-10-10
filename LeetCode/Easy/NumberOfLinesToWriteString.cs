a03e4e9f813941bf7c74d719ad5a387de86602b8
Skowoo <skowoo@tlen.pl>
2024-10-10 13:13:34 +0200

    NumberOfLinesToWriteString done

---
+++ LeetCode/Easy/NumberOfLinesToWriteString.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    internal class NumberOfLinesToWriteString
+    {
+        public static int[] NumberOfLines(int[] widths, string s)
+        {
+            int result = 0,
+                currentLine = 0;
+
+            foreach (var c in s)
+            {
+                if (currentLine + widths[c - 'a'] > 100)
+                {
+                    currentLine = 0;
+                    result++;
+                }
+                currentLine += widths[c - 'a'];
+            }
+
+            return [result + 1, currentLine];
+        }
+    }
+}

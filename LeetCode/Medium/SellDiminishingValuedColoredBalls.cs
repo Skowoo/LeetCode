e2a7517ab0d87fe1248a70270a9988b8d7be7a58
Skowoo <skowoo@tlen.pl>
2024-10-11 08:52:04 +0200

    SellDiminishingValuedColoredBalls "done"

---
+++ LeetCode/Medium/SellDiminishingValuedColoredBalls.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Medium
+{
+    internal class SellDiminishingValuedColoredBalls
+    {
+        public static int MaxProfit(int[] inventory, int orders)
+        {
+            var (result, min, max) = (0L, 1L, (long)inventory.Max());
+            while (min <= max)
+            {
+                long middle = min + (max - min) / 2;
+                var (sum, ballsToSell) = (0L, 0L);
+                foreach (var n in inventory)
+                {
+                    if (n < middle)
+                        continue;
+
+                    long balls = n - middle + 1;
+                    ballsToSell += balls;
+                    sum += balls * (2 * middle + (balls - 1) * 1) / 2;
+                }
+
+                if (ballsToSell < orders)
+                {
+                    max = middle - 1;
+                    continue;
+                }
+
+                (result, min) = (sum, middle + 1);
+                result -= (ballsToSell - orders) * middle;
+            }
+            result %= 1_000_000_007;
+            return (int)result;
+        }
+    }
+}

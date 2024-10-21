e985845a7b91c63192939a933f12ffdef087f5cd
Skowoo <skowoo@tlen.pl>
2024-10-21 19:16:35 +0200

    MaxConsecutiveOnes done

---
+++ LeetCode/Easy/MaxConsecutiveOnes.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal class MaxConsecutiveOnes
+    {
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0,
+                curr = 0;
+
+            foreach (int x in nums)
+                if (x == 1)
+                {
+                    curr++;
+                    max = Math.Max(max, curr);
+                }
+                else
+                    curr = 0;
+
+            return max;
+        }
+    }
+}

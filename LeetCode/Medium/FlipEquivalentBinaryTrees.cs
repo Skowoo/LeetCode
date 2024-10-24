5bf1662d609fcf1d1d56da54803db710263e32af
Skowoo <skowoo@tlen.pl>
2024-10-24 11:54:52 +0200

    FlipEquivalentBinaryTrees done

---
+++ LeetCode/Medium/FlipEquivalentBinaryTrees.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal class FlipEquivalentBinaryTrees
+    {
+        public static bool FlipEquiv(TreeNode root1, TreeNode root2)
+        {
+            if (root1 is null || root2 is null)
+                return root1 is null && root2 is null;
+
+            if (root1.val != root2.val)
+                return false;
+
+            return (FlipEquiv(root1.right, root2.left) && FlipEquiv(root1.left, root2.right))
+                || (FlipEquiv(root1.right, root2.right) && FlipEquiv(root1.left, root2.left));
+        }
+    }
+}

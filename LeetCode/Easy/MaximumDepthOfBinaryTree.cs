af7c03a4f8b3bbe15a3fbd644b253a1c1454c3ba
Skowoo <skowoo@tlen.pl>
2024-10-25 10:54:30 +0200

    MaximumDepthOfBinaryTree done

---
+++ LeetCode/Easy/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal class MaximumDepthOfBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root is null)
+                return 0;
+
+            int left = MaxDepth(root.left),
+                right = MaxDepth(root.right);
+
+            return Math.Max(left, right) + 1;
+        }
+    }
+}

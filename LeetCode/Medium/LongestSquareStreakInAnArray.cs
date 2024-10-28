34663f77dee3a3f3948a60e8e9470b9fc8e157d4
Skowoo <skowoo@tlen.pl>
2024-10-28 18:41:14 +0100

    LongestSquareStreakInAnArray done

---
+++ LeetCode/Medium/LongestSquareStreakInAnArray.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Medium
+{
+    internal class LongestSquareStreakInAnArray
+    {
+        public static int LongestSquareStreak(int[] nums)
+        {
+            HashSet<int> set = new(nums);
+            int max = -1;
+
+            foreach (int num in set)
+                if (set.Contains((int)Math.Pow(num, 2)))
+                {
+                    int counter = 2,
+                        squared = (int)Math.Pow(num, 2);
+
+                    while (true)
+                    {
+                        if (set.Contains((int)Math.Pow(squared, 2)))
+                        {
+                            squared = (int)Math.Pow(squared, 2);
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    max = Math.Max(max, counter);
+                }
+
+            return max;
+        }
+    }
+}
+
+//Console.WriteLine(LongestSquareStreak([4, 3, 6, 16, 8, 2]));
+//Console.WriteLine(LongestSquareStreak([2, 3, 5, 6, 7]));
+//Console.WriteLine(LongestSquareStreak([2, 49, 3, 5, 7, 2401]));

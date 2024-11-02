45e3275874abd6081d33fb01a6387027170d5b04
Skowoo <skowoo@tlen.pl>
2024-11-02 16:22:44 +0100

    CircularSentence done

---
+++ LeetCode/Easy/CircularSentence.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Easy
+{
+    internal class CircularSentence
+    {
+        public static bool IsCircularSentence(string sentence)
+        {
+            var words = sentence.Split(' ');
+            if (sentence[0] != sentence[^1])
+                return false;
+
+            for (int i = 1; i < words.Length; i++)
+                if (words[i - 1][^1] != words[i][0])
+                    return false;
+
+            return true;
+        }
+    }
+}

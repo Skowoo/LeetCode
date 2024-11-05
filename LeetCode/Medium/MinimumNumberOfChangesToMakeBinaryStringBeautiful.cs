284ed12693ae9405ad2ccbdb230b7e8127242afe
Skowoo <skowoo@tlen.pl>
2024-11-05 08:45:56 +0100

    Program class update MinimumNumberOfChangesToMakeBinaryStringBeautiful done

---
+++ LeetCode/Medium/MinimumNumberOfChangesToMakeBinaryStringBeautiful.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Medium
+{
+    internal class MinimumNumberOfChangesToMakeBinaryStringBeautiful
+    {
+        public static int MinChanges(string s)
+        {
+            int result = 0;
+
+            for (int i = 1; i < s.Length; i += 2)
+                if (s[i] != s[i - 1])
+                    result++;
+
+            return result;
+        }
+    }
+}

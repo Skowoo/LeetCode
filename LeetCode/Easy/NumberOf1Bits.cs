d31cdf0645c49e91bca77b0e7746329904d86eed
Skowoo <skowoo@tlen.pl>
2024-11-07 12:09:15 +0100

    NumberOf1Bits done

---
+++ LeetCode/Easy/NumberOf1Bits.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal class NumberOf1Bits
+    {
+        public static int HammingWeight(int n)
+        {
+            int result = 0;
+            while (n > 0)
+            {
+                result += n & 1;
+                n >>= 1;
+            }
+
+            return result;
+        }
+    }
+}

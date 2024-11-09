799ec865e00081b982ea3afb8fa2099bba7951a0
Skowoo <skowoo@tlen.pl>
2024-11-09 08:01:01 +0100

    ValidAnagram done

---
+++ LeetCode/Easy/ValidAnagram.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    internal class ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            var arrayA = s.ToCharArray();
+            var arrayB = t.ToCharArray();
+            Array.Sort(arrayA);
+            Array.Sort(arrayB);
+            return new String(arrayA).Equals(new String(arrayB));
+        }
+    }
+}

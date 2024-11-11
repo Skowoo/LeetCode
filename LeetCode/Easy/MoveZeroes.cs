eff4af9b53d6c72692eb71c659226056334a2bc3
Skowoo <skowoo@tlen.pl>
2024-11-11 09:23:59 +0100

    MoveZeroes done

---
+++ LeetCode/Easy/MoveZeroes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class MoveZeroes
+    {
+        public void MoveZeroesFunc(int[] nums) // Wrong solution - pointer issue.
+        {
+            var usableLength = nums.Length;
+            var numsList = new List<int>(nums);
+            for (int i = 0; i < usableLength; i++)
+                if (nums[i] == 0)
+                {
+                    numsList.RemoveAt(i);
+                    numsList.Add(0);
+                    usableLength--;
+                }
+
+            nums = [.. numsList];
+        }
+    }
+}

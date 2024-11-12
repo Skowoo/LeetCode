81cdd9d420274fd7235a5a78cc3d1f9d6b1e67b3
Skowoo <skowoo@tlen.pl>
2024-11-12 06:35:43 +0100

    NumberOfDaysBetweenTwoDates done

---
+++ LeetCode/Easy/NumberOfDaysBetweenTwoDates.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal class NumberOfDaysBetweenTwoDates
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S6562", Justification = "Simple solution")]
+        public static int DaysBetweenDates(string date1, string date2)
+        {
+            var firstDateComponents = date1.Split('-');
+            var secondDateComponents = date2.Split('-');
+            var firstDate = new DateTime(int.Parse(firstDateComponents[0]), int.Parse(firstDateComponents[1]), int.Parse(firstDateComponents[2]));
+            var secondDate = new DateTime(int.Parse(secondDateComponents[0]), int.Parse(secondDateComponents[1]), int.Parse(secondDateComponents[2]));
+            var timespan = secondDate - firstDate;
+            return Math.Abs(timespan.Days);
+        }
+    }
+}

c4f8eeaf252c1ff3b7791cbe5f639a319ed5be16
Skowoo <skowoo@tlen.pl>
2024-11-13 18:29:37 +0100

    FizzBuzz done

---
+++ LeetCode/Easy/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal class FizzBuzz
+    {
+        public static IList<string> FizzBuzzFunc(int n)
+        {
+            List<string> result = [];
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    result.Add("FizzBuzz");
+                else if (i % 3 == 0)
+                    result.Add("Fizz");
+                else if (i % 5 == 0)
+                    result.Add("Buzz");
+                else
+                    result.Add(i.ToString());
+            }
+            return result;
+        }
+    }
+}

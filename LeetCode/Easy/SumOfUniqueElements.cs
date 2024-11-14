f93ecd625430c29f05988520ac22e1d870e7444c
Skowoo <skowoo@tlen.pl>
2024-11-14 06:30:53 +0100

    SumOfUniqueElements done

---
+++ LeetCode/Easy/SumOfUniqueElements.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SumOfUniqueElements
+    {
+        public static int SumOfUnique(int[] nums)
+        {
+            Dictionary<int, int> dic = [];
+            foreach (int x in nums)
+                if (dic.TryGetValue(x, out int result))
+                    dic[x] = result + 1;
+                else
+                    dic[x] = 1;
+
+            return dic.Where(x => x.Value == 1).Select(x => x.Key).Sum();
+        }
+    }
+}

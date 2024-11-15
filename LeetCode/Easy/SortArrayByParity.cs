59f1228e082018c8f192fb73556f13e0cab459bf
Skowoo <skowoo@tlen.pl>
2024-11-15 06:30:28 +0100

    SortArrayByParity done

---
+++ LeetCode/Easy/SortArrayByParity.cs
@@ -0,0 +1,7 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SortArrayByParity
+    {
+        public static int[] SortArrayByParityFunc(int[] nums) => nums.Where(x => x % 2 == 0).Concat(nums.Where(x => x % 2 == 1)).ToArray();
+    }
+}

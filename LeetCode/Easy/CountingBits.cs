3e7edfb53397e588afc36fb77fa7c103c0658744
Skowoo <skowoo@tlen.pl>
2024-11-16 07:58:28 +0100

    CountingBits done

---
+++ LeetCode/Easy/CountingBits.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CountingBits
+    {
+        public static int[] CountBits(int n)
+        {
+            var result = new int[n + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                int count = 0,
+                    num = i;
+
+                while (num > 0)
+                {
+                    count += num & 1;
+                    num >>= 1;
+                }
+
+                result[i] = count;
+            }
+
+            return result;
+        }
+    }
+}

cb6f51bd9d7e8660b7bc5bd1fd28c0a2004a8d0f
Skowoo <skowoo@tlen.pl>
2024-11-19 06:26:12 +0100

    AddDigits done

---
+++ LeetCode/Easy/AddDigits.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class AddDigits
+    {
+        public static int AddDigitsFunc(int num)
+        {
+            while (num > 9)
+                num = num
+                    .ToString()
+                    .ToCharArray()
+                    .Select(c => int.Parse(c.ToString()))
+                    .Sum();
+
+            return num;
+        }
+    }
+}

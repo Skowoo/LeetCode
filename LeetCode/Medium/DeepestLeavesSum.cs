9059cec198be8e53d2edd5a190c3fe4123cd06ed
Skowoo <skowoo@tlen.pl>
2024-11-20 08:53:02 +0100

    DeepestLeavesSum done

---
+++ LeetCode/Medium/DeepestLeavesSum.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal static class DeepestLeavesSum
+    {
+        public static int DeepestLeavesSumFunc(TreeNode root)
+        {
+            List<TreeNode> currentLevel = [root],
+                nextLevel;
+
+            int currentLevelSum;
+
+            do
+            {
+                nextLevel = [];
+                currentLevelSum = 0;
+
+                foreach (var node in currentLevel)
+                {
+                    if (node.right is not null)
+                        nextLevel.Add(node.right);
+
+                    if (node.left is not null)
+                        nextLevel.Add(node.left);
+
+                    currentLevelSum += node.val;
+                }
+
+                currentLevel = nextLevel;
+            }
+            while (nextLevel.Count > 0);
+
+            return currentLevelSum;
+        }
+    }
+}

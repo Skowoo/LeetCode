4b4f9f8d57264bd50dfcf23681b9f25a35eae384
Skowoo <skowoo@tlen.pl>
2024-11-22 08:35:29 +0100

    EncodeAndDecodeTinyURL done

---
+++ LeetCode/Medium/EncodeAndDecodeTinyURL.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Medium
+{
+    internal class EncodeAndDecodeTinyURL
+    {
+        public class Codec
+        {
+            Dictionary<int, string> storedLinks = [];
+            int counter = 0;
+
+            public string encode(string longUrl)
+            {
+                counter++;
+                storedLinks.Add(counter, longUrl);
+                return counter.ToString();
+            }
+
+
+            public string decode(string shortUrl)
+            {
+                var validKey = int.TryParse(shortUrl, out int number);
+                if (validKey)
+                    return storedLinks[number];
+
+                return "Error";
+            }
+        }
+    }
+}

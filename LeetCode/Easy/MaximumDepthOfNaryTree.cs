929ab0ebf40ad95a0e33590253a6cad3200ffdc9
Skowoo <skowoo@tlen.pl>
2024-11-24 09:05:20 +0100

    MaximumDepthOfNaryTree done

---
+++ LeetCode/Easy/MaximumDepthOfNaryTree.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal static class MaximumDepthOfNaryTree
+    {
+        public static int MaxDepth(Node root)
+        {
+            return GetDepth(root, 0);
+
+            static int GetDepth(Node root, int value)
+            {
+                if (root is null)
+                    return value;
+
+                value = value + 1;
+
+                int maxDepth = value;
+
+                if (root.children is not null)
+                    foreach (var c in root.children)
+                        if (GetDepth(c, value) > maxDepth)
+                            maxDepth = GetDepth(c, value);
+
+                return maxDepth;
+            }
+        }
+    }
+}

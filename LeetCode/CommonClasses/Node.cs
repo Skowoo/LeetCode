17a49c7714b1d7b0122e3833d86c21c69981ef81
Skowoo <skowoo@tlen.pl>
2024-11-24 08:55:52 +0100

    new CommonClass added - Node.

---
+++ LeetCode/CommonClasses/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.CommonClasses
+{
+    /// <summary>
+    /// LeetCode base class which represents N-ary tree in some problems.
+    /// </summary>
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}

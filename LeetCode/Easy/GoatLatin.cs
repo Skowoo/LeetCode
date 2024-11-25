58c071f163dc26b85d4da1aeabf0f411fd3947e4
Skowoo <skowoo@tlen.pl>
2024-11-25 09:07:27 +0100

    GoatLatin done

---
+++ LeetCode/Easy/GoatLatin.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal static class GoatLatin
+    {
+        public static string ToGoatLatin(string sentence)
+        {
+            var words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            char[] vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+            for (int i = 0; i < words.Length; i++)
+            {
+                StringBuilder sb = new(words[i]);
+                if (vowels.Contains(words[i][0]))
+                    sb.Append("ma");
+                else
+                {
+                    sb.Append($"{words[i][0]}ma");
+                    sb.Remove(0, 1);
+                }
+
+                for (int j = 0; j < i + 1; j++)
+                    sb.Append('a');
+
+                words[i] = sb.ToString();
+            }
+
+            return String.Join(" ", words);
+        }
+    }
+}

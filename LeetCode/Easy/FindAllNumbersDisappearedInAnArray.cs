9e1fd3afca5068d806e1d9a40e826c7fc23c6130
Skowoo <skowoo@tlen.pl>
2024-11-28 06:32:33 +0100

    FindAllNumbersDisappearedInAnArray done

---
+++ LeetCode/Easy/FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class FindAllNumbersDisappearedInAnArray
+    {
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            int[] re = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+                re[i] = i + 1;
+
+            return re.Except(nums).ToList();
+        }
+    }
+}

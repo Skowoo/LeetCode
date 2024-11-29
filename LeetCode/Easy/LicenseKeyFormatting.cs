fe869fd9916654d87194f10988bf896b096cb60e
Skowoo <skowoo@tlen.pl>
2024-11-29 06:32:01 +0100

    LicenseKeyFormatting done

---
+++ LeetCode/Easy/LicenseKeyFormatting.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal static class LicenseKeyFormatting
+    {
+        public static string LicenseKeyFormattingFunc(string s, int k)
+        {
+            var clean = s.Replace("-", "").ToUpper();
+            StringBuilder sb = new();
+            int counter = 0;
+            for (int i = clean.Length - 1; i >= 0; i--)
+            {
+
+                counter++;
+
+                sb.Insert(0, clean[i]);
+
+                if (counter == k && i != 0)
+                {
+                    counter = 0;
+                    sb.Insert(0, '-');
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}

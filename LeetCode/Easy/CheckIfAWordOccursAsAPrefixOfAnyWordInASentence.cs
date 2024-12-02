f3e7d3e178bda852353a21dc6ba43d65e9c7b4ef
Skowoo <skowoo@tlen.pl>
2024-12-02 09:08:35 +0100

    CheckIfAWordOccursAsAPrefixOfAnyWordInASentence done

---
+++ LeetCode/Easy/CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CheckIfAWordOccursAsAPrefixOfAnyWordInASentence
+    {
+        public static int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            int result = -1;
+            var words = sentence.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+                for (int j = 0; j <= words[i].Length; j++)
+                {
+                    if (j == searchWord.Length)
+                        return i + 1;
+
+                    if (j == words[i].Length)
+                        break;
+
+                    if (searchWord[j] != words[i][j])
+                        break;
+                }
+
+            return result;
+        }
+    }
+}

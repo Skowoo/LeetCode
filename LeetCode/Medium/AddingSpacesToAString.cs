b7e4d419fc38d7572dcb94209d545b6c8ce6c850
Skowoo <skowoo@tlen.pl>
2024-12-03 21:58:10 +0100

    AddingSpacesToAString done

---
+++ LeetCode/Medium/AddingSpacesToAString.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace LeetCode.Medium
+{
+    internal static class AddingSpacesToAString
+    {
+        public static string AddSpaces(string s, int[] spaces)
+        {
+            StringBuilder sb = new(s);
+            for (int i = 0; i < spaces.Length; i++)
+                sb.Insert(i + spaces[i], ' ');
+
+            return sb.ToString();
+        }
+    }
+}

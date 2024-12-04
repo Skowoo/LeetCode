c2cf79e2e5ee1477b5ea1e4311deb9b3a79db265
Skowoo <skowoo@tlen.pl>
2024-12-04 11:01:19 +0100

    FindSmallestLetterGreaterThanTarget done

---
+++ LeetCode/Easy/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class FindSmallestLetterGreaterThanTarget
+    {
+        public static char NextGreatestLetter(char[] letters, char target)
+        {
+            if (letters[letters.Length - 1] < target)
+                return letters[0];
+
+            for (int i = 0; i < letters.Length; i++)
+                if (target < letters[i])
+                    return letters[i];
+
+            return letters[0];
+        }
+    }
+}

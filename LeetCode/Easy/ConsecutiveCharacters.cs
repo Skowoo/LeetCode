ef788b10aeb70368ddc79b1b986d935262375a45
Skowoo <skowoo@tlen.pl>
2024-12-05 10:17:33 +0100

    ConsecutiveCharacters done

---
+++ LeetCode/Easy/ConsecutiveCharacters.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ConsecutiveCharacters
+    {
+        public static int MaxPower(string s)
+        {
+            int current = 0,
+                max = 0;
+            char prev = s[0];
+            foreach (char c in s)
+            {
+                if (c == prev)
+                    current++;
+                else
+                {
+                    prev = c;
+                    max = Math.Max(current, max);
+                    current = 1;
+                }
+            }
+            return Math.Max(current, max);
+        }
+    }
+}

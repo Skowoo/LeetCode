3d8abace5a1444d0587cc06a045456ed65dd83eb
Skowoo <skowoo@tlen.pl>
2024-12-06 09:00:07 +0100

    CountAsterisks done

---
+++ LeetCode/Easy/CountAsterisks.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CountAsterisks
+    {
+        public static int CountAsterisksFunc(string s)
+        {
+            var words = s.Split('|');
+            List<string> validWords = [];
+            bool sw = true;
+            foreach (var word in words)
+            {
+                if (sw)
+                    validWords.Add(word);
+                sw = !sw;
+            }
+            var validChars = String.Join("", validWords).ToCharArray();
+            return validChars.Count(x => x == '*');
+        }
+    }
+}

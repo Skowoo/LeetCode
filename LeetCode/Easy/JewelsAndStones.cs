1a3d0bbbc1e947cfe947a7f5d9cfcbf343d300d5
Skowoo <skowoo@tlen.pl>
2024-12-07 11:32:59 +0100

    JewelsAndStones done

---
+++ LeetCode/Easy/JewelsAndStones.cs
@@ -0,0 +1,8 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class JewelsAndStones
+    {
+        public static int NumJewelsInStones(string jewels, string stones)
+            => stones.ToCharArray().Count(x => jewels.ToCharArray().Contains(x));
+    }
+}

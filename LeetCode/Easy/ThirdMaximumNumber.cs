51456b903d1ae19168afdfb6a56e1e7e615db234
Skowoo <skowoo@tlen.pl>
2024-12-08 08:22:17 +0100

    ThirdMaximumNumber done

---
+++ LeetCode/Easy/ThirdMaximumNumber.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ThirdMaximumNumber
+    {
+        public static int ThirdMax(int[] nums)
+        {
+            var distinctNumbers = new HashSet<int>(nums).OrderByDescending(x => x);
+            return distinctNumbers.Count() >= 3 ? distinctNumbers.Skip(2).First() : distinctNumbers.First();
+        }
+    }
+}

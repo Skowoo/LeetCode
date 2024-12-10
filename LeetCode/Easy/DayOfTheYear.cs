069acc46f704b649133ade66a5dbf67bb78a0858
Skowoo <skowoo@tlen.pl>
2024-12-10 06:27:20 +0100

    DayOfTheYear done

---
+++ LeetCode/Easy/DayOfTheYear.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class DayOfTheYear
+    {
+        public static int DayOfYear(string date)
+        {
+            var firstDay = new DateTime(int.Parse(date.Split('-')[0]), 01, 01);
+            var currentDay = DateTime.Parse(date);
+            return (currentDay - firstDay).Days + 1;
+        }
+    }
+}

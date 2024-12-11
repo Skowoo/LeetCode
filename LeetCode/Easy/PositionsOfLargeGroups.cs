a5bdbac080332b905e0a0dcb921b82d38fa0aa44
Skowoo <skowoo@tlen.pl>
2024-12-11 06:31:28 +0100

    PositionsOfLargeGroups done

---
+++ LeetCode/Easy/PositionsOfLargeGroups.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class PositionsOfLargeGroups
+    {
+        public static IList<IList<int>> LargeGroupPositions(string s)
+        {
+            List<IList<int>> result = [];
+            int i = 0;
+
+            while (i < s.Length)
+            {
+                int firstCharIndex = i;
+
+                while (i < s.Length && s[i] == s[firstCharIndex])
+                    i++;
+
+                if (i - firstCharIndex >= 3)
+                    result.Add([firstCharIndex, i - 1]);
+            }
+
+            return result;
+        }
+    }
+}

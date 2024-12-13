6d1786058a660bbfab2242b941c55405c487b814
Skowoo <skowoo@tlen.pl>
2024-12-13 09:09:04 +0100

    MostCommonWord done

---
+++ LeetCode/Easy/MostCommonWord.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    public static class MostCommonWord
+    {
+        public static string MostCommonWord_Func(string paragraph, string[] banned)
+        {
+            StringBuilder sb = new(paragraph);
+            for (int i = 0; i < sb.Length; i++)
+                if (!char.IsLetter(sb[i]))
+                    sb[i] = ' ';
+
+            var words = sb.ToString()
+                .ToLower()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            Dictionary<string, int> keyValuePairs = [];
+
+            foreach (var word in words)
+            {
+                if (banned.Contains(word))
+                    continue;
+
+                if (keyValuePairs.ContainsKey(word))
+                    keyValuePairs[word]++;
+                else
+                    keyValuePairs[word] = 1;
+            }
+
+            return keyValuePairs.OrderByDescending(x => x.Value).First().Key;
+        }
+    }
+}

f76d291479c61040f4650673106fd4f1cd31d770
Skowoo <skowoo@tlen.pl>
2024-12-14 09:47:29 +0100

    CountCompleteTreeNodes done

---
+++ LeetCode/Easy/CountCompleteTreeNodes.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal static class CountCompleteTreeNodes
+    {
+        public static int CountNodes(TreeNode root)
+        {
+            int result = 0;
+
+            IsNode(root);
+
+            void IsNode(TreeNode node)
+            {
+                if (node is null)
+                    return;
+
+                if (node.left is not null)
+                    IsNode(node.left);
+
+                if (node.right is not null)
+                    IsNode(node.right);
+
+                result++;
+            }
+
+            return result;
+        }
+    }
+}

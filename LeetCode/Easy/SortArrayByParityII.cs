f0295907ea39ad555eaa504a973fddc7ad54ed20
Skowoo <skowoo@tlen.pl>
2024-12-15 08:27:10 +0100

    SortArrayByParityII done

---
+++ LeetCode/Easy/SortArrayByParityII.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SortArrayByParityII
+    {
+        public static int[] SortArrayByParityII_Func(int[] nums)
+        {
+            int oddIndex = 1,
+                evenIndex = 0;
+
+            int[] result = new int[nums.Length];
+
+            foreach (int i in nums)
+                if (i % 2 == 0)
+                {
+                    result[evenIndex] = i;
+                    evenIndex += 2;
+                }
+                else
+                {
+                    result[oddIndex] = i;
+                    oddIndex += 2;
+                }
+
+            return result;
+        }
+    }
+}

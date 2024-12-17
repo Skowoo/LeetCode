58b81de55ffe9a01e40aaa5fc1ed848006f2b67f
Skowoo <skowoo@tlen.pl>
2024-12-17 06:22:59 +0100

    RobotReturnToOrigin done

---
+++ LeetCode/Easy/RobotReturnToOrigin.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class RobotReturnToOrigin
+    {
+        public static bool JudgeCircle(string moves)
+        {
+            int horizontal = 0,
+                vertical = 0;
+
+            foreach (var move in moves)
+                if (move == 'U')
+                    vertical++;
+                else if (move == 'D')
+                    vertical--;
+                else if (move == 'R')
+                    horizontal++;
+                else if (move == 'L')
+                    horizontal--;
+
+            return horizontal == vertical && vertical == 0;
+        }
+    }
+}

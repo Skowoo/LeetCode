ce86c96074954de3845b7007215d7f77153de15a
Skowoo <skowoo@tlen.pl>
2024-12-19 06:29:26 +0100

    FindPivotIndex done

---
+++ LeetCode/Easy/FindPivotIndex.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class FindPivotIndex
+    {
+        public static int PivotIndex(int[] nums)
+        {
+            int totalSum = nums.Sum(),
+                currSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                totalSum -= nums[i];
+
+                if (currSum == totalSum)
+                    return i;
+
+                currSum += nums[i];
+
+                if (currSum > totalSum)
+                    break;
+            }
+
+            return -1;
+        }
+    }
+}

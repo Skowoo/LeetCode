e2c2cd592546c71019771505243ed2d50308a29c
Skowoo <skowoo@tlen.pl>
2024-12-20 06:25:52 +0100

    RichestCustomerWealth done

---
+++ LeetCode/Easy/RichestCustomerWealth.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class RichestCustomerWealth
+    {
+        public static int MaximumWealth(int[][] accounts)
+        {
+            int max = 0;
+            foreach (var account in accounts)
+            {
+                int curValue = 0;
+                foreach (var item in account)
+                    curValue += item;
+
+                max = Math.Max(max, curValue);
+            }
+            return max;
+        }
+
+        public static int MaximumWealth_Slow(int[][] accounts)
+            => accounts.Select(a => a.Sum()).Max();
+    }
+}

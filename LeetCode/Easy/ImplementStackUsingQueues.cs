720720330603c60b3c37ed2e77dd675670de4832
Skowoo <skowoo@tlen.pl>
2024-12-21 07:32:55 +0100

    ImplementStackUsingQueues done

---
+++ LeetCode/Easy/ImplementStackUsingQueues.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ImplementStackUsingQueues
+    {
+        public class MyStack
+        {
+            readonly private Queue<int> queue = [];
+
+            public void Push(int x)
+            {
+                queue.Enqueue(x);
+                for (int i = 0; i < queue.Count - 1; i++)
+                    queue.Enqueue(queue.Dequeue());
+            }
+
+            public int Pop() => queue.Dequeue();
+
+            public int Top() => queue.Peek();
+
+            public bool Empty() => queue.Count == 0;
+        }
+    }
+}

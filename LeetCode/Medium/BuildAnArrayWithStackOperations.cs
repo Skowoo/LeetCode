9688f3c121ddbc403a0f6ce51153d5608adc8d5a
Skowoo <skowoo@tlen.pl>
2024-12-22 08:05:16 +0100

    BuildAnArrayWithStackOperations done

---
+++ LeetCode/Medium/BuildAnArrayWithStackOperations.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class BuildAnArrayWithStackOperations
+    {
+        public static IList<string> BuildArray(int[] target, int n)
+        {
+            HashSet<int> hashSet = new(target);
+            List<string> result = [];
+
+            for (int i = 0; i < target[^1]; i++)
+            {
+                result.Add("Push");
+
+                if (!hashSet.Contains(i + 1))
+                    result.Add("Pop");
+            }
+
+            return result;
+        }
+    }
+}

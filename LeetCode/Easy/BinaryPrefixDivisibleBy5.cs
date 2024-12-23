484082ff575d9c94e031d2964ea0879ec712fe81
Skowoo <skowoo@tlen.pl>
2024-12-23 07:51:37 +0100

    BinaryPrefixDivisibleBy5 done

---
+++ LeetCode/Easy/BinaryPrefixDivisibleBy5.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class BinaryPrefixDivisibleBy5
+    {
+        public static IList<bool> PrefixesDivBy5(int[] nums)
+        {
+            bool[] result = new bool[nums.Length];
+            int currNum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currNum = (currNum * 2 + nums[i]) % 5;
+                result[i] = currNum == 0;
+            }
+
+            return result;
+        }
+    }
+}

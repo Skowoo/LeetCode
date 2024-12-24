917ae987d9417a1e48a5f2b0d3f864e862613221
Skowoo <skowoo@tlen.pl>
2024-12-24 06:54:39 +0100

    KeyboardRow done

---
+++ LeetCode/Easy/KeyboardRow.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class KeyboardRow
+    {
+        public static string[] FindWords(string[] words)
+        {
+            string[] rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+            List<string> result = [];
+            foreach (string word in words)
+            {
+                int rowNumber = 0;
+                for (int i = 0; i < rows.Length; i++)
+                    if (rows[i].Contains(word.ToLower()[0]))
+                        rowNumber = i;
+
+                bool isAccepted = true;
+                foreach (char ch in word.ToLower())
+                    if (!rows[rowNumber].Contains(ch))
+                    {
+                        isAccepted = false;
+                        break;
+                    }
+
+                if (isAccepted)
+                    result.Add(word);
+            }
+
+            return [.. result];
+        }
+    }
+}

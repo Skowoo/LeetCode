b2869a39b2e4578ac2918eeafbc5515b4c2c8365
Skowoo <skowoo@tlen.pl>
2024-12-25 08:01:35 +0100

    FindLargestValueInEachTreeRow done

---
+++ LeetCode/Medium/FindLargestValueInEachTreeRow.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Medium
+{
+    internal static class FindLargestValueInEachTreeRow
+    {
+        public static IList<int> LargestValues(TreeNode root)
+        {
+            if (root is null)
+                return [];
+
+            List<int> result = [];
+            List<TreeNode> currentLevel = [root],
+                nextLevel = [];
+
+            while (currentLevel.Count > 0)
+            {
+                int currentMax = int.MinValue;
+
+                foreach (TreeNode node in currentLevel)
+                {
+                    if (node.left is not null)
+                        nextLevel.Add(node.left);
+
+                    if (node.right is not null)
+                        nextLevel.Add(node.right);
+
+                    currentMax = Math.Max(currentMax, node.val);
+                }
+
+                result.Add(currentMax);
+                currentLevel = nextLevel;
+                nextLevel = [];
+            }
+
+            return result;
+        }
+    }
+}

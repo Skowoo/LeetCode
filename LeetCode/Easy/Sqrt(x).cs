f7f28dc128e527b139107344cac742b0b1083019
Skowoo <skowoo@tlen.pl>
2024-12-26 08:27:44 +0100

    Sqrt(x) done

---
+++ LeetCode/Easy/Sqrt(x).cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class Sqrt_x_
+    {
+        public static int MySqrt(int x)
+        {
+            if (x < 2)
+                return x;
+
+            double cdt = 1;
+            while (true)
+            {
+                if ((cdt * cdt) <= x && ((cdt + 1) * (cdt + 1)) > x)
+                    return (int)cdt;
+
+                cdt++;
+            }
+        }
+    }
+}

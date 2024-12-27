5880fe81401916d4c3626c01ace25e3f3cf114d8
Skowoo <skowoo@tlen.pl>
2024-12-27 08:36:31 +0100

    SingleNumber done

---
+++ LeetCode/Easy/SingleNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SingleNumber
+    {
+        public static int SingleNumberFunc(int[] nums)
+        {
+            int result = 0;
+            foreach (int x in nums)
+                result ^= x;
+
+            return result;
+        }
+    }
+}

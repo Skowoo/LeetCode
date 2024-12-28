04ad91c60df1c8a3117824aae551ad136cd4d43e
Skowoo <skowoo@tlen.pl>
2024-12-28 08:16:16 +0100

    FindLuckyIntegerInAnArray done

---
+++ LeetCode/Easy/FindLuckyIntegerInAnArray.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class FindLuckyIntegerInAnArray
+    {
+        public static int FindLucky(int[] arr)
+        {
+            Dictionary<int, int> map = [];
+            foreach (int i in arr)
+                if (map.TryGetValue(i, out int value))
+                    map[i] = ++value;
+                else
+                    map.Add(i, 1);
+
+            int result = -1;
+            foreach (var entry in map)
+                if (entry.Key == entry.Value && entry.Value > result)
+                    result = entry.Value;
+
+            return result;
+        }
+    }
+}

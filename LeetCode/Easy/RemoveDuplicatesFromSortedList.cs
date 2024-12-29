31808c49104533c8ade815714dc1fe8651fdabb8
Skowoo <skowoo@tlen.pl>
2024-12-29 08:27:20 +0100

    RemoveDuplicatesFromSortedList done

---
+++ LeetCode/Easy/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.CommonClasses;
+
+namespace LeetCode.Easy
+{
+    internal static class RemoveDuplicatesFromSortedList
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head is null)
+                return null!;
+
+            var pointer = head;
+
+            while (pointer.next is not null)
+                if (pointer.val == pointer.next.val)
+                    pointer.next = pointer.next.next;
+                else
+                    pointer = pointer.next;
+
+            return head;
+        }
+    }
+}

04d4071ea759efbb293cb02b2d7d0caed24db738
Skowoo <skowoo@tlen.pl>
2024-12-30 08:36:45 +0100

    XorOperationInAnArray done

---
+++ LeetCode/Easy/XOROperationInAnArray.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class XorOperationInAnArray
+    {
+        public static int XorOperation(int n, int start)
+        {
+            int result = start;
+
+            for (int i = 1; i < n; i++)
+                result ^= start + 2 * i;
+
+            return result;
+        }
+    }
+}

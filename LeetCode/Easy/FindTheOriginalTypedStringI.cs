bf62984573a69f75dc207c84577338a3fbea4c84
Skowoo <skowoo@tlen.pl>
2024-12-31 08:18:41 +0100

    FindTheOriginalTypedStringI done

---
+++ LeetCode/Easy/FindTheOriginalTypedStringI.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class FindTheOriginalTypedStringI
+    {
+        public static int PossibleStringCount(string word)
+        {
+            int result = 1;
+
+            for (int i = 1; i < word.Length; i++)
+                if (word[i] == word[i - 1])
+                    result++;
+
+            return result;
+        }
+    }
+}

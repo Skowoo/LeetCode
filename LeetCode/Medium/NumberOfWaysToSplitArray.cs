8900e202fce8d71470d0be0d235443d168efd732
Skowoo <skowoo@tlen.pl>
2025-01-03 09:00:03 +0100

    NumberOfWaysToSplitArray done

---
+++ LeetCode/Medium/NumberOfWaysToSplitArray.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class NumberOfWaysToSplitArray
+    {
+        public static int WaysToSplitArray(int[] nums)
+        {
+            long totalSum = 0,
+                currSum = 0;
+            int result = 0;
+
+            foreach (int x in nums)
+                totalSum += x;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                currSum += nums[i];
+                totalSum -= nums[i];
+
+                if (currSum >= totalSum)
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}

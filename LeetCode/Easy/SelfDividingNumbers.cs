99ffc6fe863ea99cf2b59e3ea8fcd9ba21bd95e1
Skowoo <skowoo@tlen.pl>
2025-01-06 07:42:58 +0100

    SelfDividingNumbers done

---
+++ LeetCode/Easy/SelfDividingNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SelfDividingNumbers
+    {
+        public static IList<int> SelfDividingNumbers_Func(int left, int right)
+        {
+            List<int> result = [];
+
+            for (int i = left; i <= right; i++)
+            {
+                bool isValid = true;
+                var digits = i.ToString()
+                    .Select(x => int.Parse(x.ToString()))
+                    .ToArray();
+
+                foreach (var digit in digits)
+                    if (digit == 0 || i % digit != 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+
+                if (isValid)
+                    result.Add(i);
+            }
+
+            return result;
+        }
+    }
+}

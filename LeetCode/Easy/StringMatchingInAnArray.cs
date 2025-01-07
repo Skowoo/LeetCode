403f0663e9d9db9f22aaff00303713a927d7d7c5
Skowoo <skowoo@tlen.pl>
2025-01-07 06:13:34 +0100

    StringMatchingInAnArray done

---
+++ LeetCode/Easy/StringMatchingInAnArray.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class StringMatchingInAnArray
+    {
+        public static IList<string> StringMatching(string[] words)
+        {
+            List<string> result = [];
+
+            for (int i = 0; i < words.Length; i++)
+                for (int j = 0; j < words.Length; j++)
+                    if (i != j && words[j].Contains(words[i]))
+                    {
+                        result.Add(words[i]);
+                        break;
+                    }
+
+            return result;
+        }
+    }
+}

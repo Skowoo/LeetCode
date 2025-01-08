d282960999400345fd4027004a1247280ed68a78
Skowoo <skowoo@tlen.pl>
2025-01-08 06:06:15 +0100

    CountPrefixAndSuffixPairsI done

---
+++ LeetCode/Easy/CountPrefixAndSuffixPairsI.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CountPrefixAndSuffixPairsI
+    {
+        public static int CountPrefixSuffixPairs(string[] words)
+        {
+            HashSet<string> fixes = [];
+            int result = 0;
+
+            foreach (var word in words)
+                for (int len = 1; len <= word.Length; len++)
+                {
+                    fixes.Add(word[..len]);
+                    fixes.Add(word[^len..]);
+                }
+
+            for (int i = 0; i < words.Length; i++)
+                for (int j = i + 1; j < words.Length; j++)
+                    if (fixes.Contains(words[j]) && words[j].StartsWith(words[i]) && words[j].EndsWith(words[i]))
+                        result++;
+
+            return result;
+        }
+    }
+}

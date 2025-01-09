52d2a4c7df20828c5709477c885b1642449f44a1
Skowoo <skowoo@tlen.pl>
2025-01-09 06:05:08 +0100

    CountingWordsWithAGivenPrefix done

---
+++ LeetCode/Easy/CountingWordsWithAGivenPrefix.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CountingWordsWithAGivenPrefix
+    {
+        public static int PrefixCount(string[] words, string pref)
+        {
+            return words.Count(x => x.StartsWith(pref));
+        }
+    }
+}

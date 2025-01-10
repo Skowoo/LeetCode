df65b84ca6155a8f324d5c34605e055d830c69c3
Skowoo <skowoo@tlen.pl>
2025-01-10 06:11:16 +0100

    DecompressRunLengthEncodedList done

---
+++ LeetCode/DecompressRunLengthEncodedList.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    internal static class DecompressRunLengthEncodedList
+    {
+        public static int[] DecompressRLElist(int[] nums)
+        {
+            List<int> result = [];
+
+            for (int i = 0; i < nums.Length; i += 2)
+                for (int j = 0; j < nums[i]; j++)
+                    result.Add(nums[i + 1]);
+
+            return [.. result];
+        }
+    }
+}

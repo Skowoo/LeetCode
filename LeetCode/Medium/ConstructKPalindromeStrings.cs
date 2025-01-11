e1c1726dbbec9ddee81e6c5079c916c57394785c
Skowoo <skowoo@tlen.pl>
2025-01-11 08:22:02 +0100

    ConstructKPalindromeStrings done

---
+++ LeetCode/Medium/ConstructKPalindromeStrings.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class ConstructKPalindromeStrings
+    {
+        public static bool CanConstruct(string s, int k)
+        {
+            if (s.Length < k)
+                return false;
+
+            int[] chars = new int[26];
+            foreach (char c in s)
+                chars[c - 97]++;
+
+            int oddCounter = 0;
+            foreach (int c in chars)
+                if (c % 2 == 1)
+                    oddCounter++;
+
+            return oddCounter < k;
+        }
+    }
+}

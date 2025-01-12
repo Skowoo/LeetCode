0a1588086a8a994b5ef711bdaf470200f2f341dc
Skowoo <skowoo@tlen.pl>
2025-01-12 08:50:26 +0100

    SimplifyPath done

---
+++ LeetCode/Medium/SimplifyPath.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class SimplifyPath
+    {
+        public static string SimplifyPath_Func(string path)
+        {
+            var parts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            List<string> result = [];
+
+            foreach (var part in parts)
+            {
+                if (part == ".." && result.Count > 0)
+                    result.RemoveAt(result.Count - 1);
+                else if (part == ".." || part == ".")
+                    continue;
+                else
+                    result.Add(part);
+            }
+
+            return $"/{string.Join('/', result)}";
+        }
+    }
+}

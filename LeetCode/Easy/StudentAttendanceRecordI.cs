517e479e45b5ac3edbdb942a8ff91c9fa3ef874c
Skowoo <skowoo@tlen.pl>
2025-01-13 06:12:47 +0100

    StudentAttendanceRecordI done

---
+++ LeetCode/Easy/StudentAttendanceRecordI.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class StudentAttendanceRecordI
+    {
+        public static bool CheckRecord(string s)
+        {
+            bool absenceCriteria = true;
+            bool lateCriteria = true;
+            int absenceCounter = 0;
+            int lateCounter = 0;
+
+            foreach (char c in s)
+                switch (c)
+                {
+                    case 'A':
+                        lateCounter = 0;
+                        absenceCounter++;
+                        if (absenceCounter >= 2)
+                            absenceCriteria = false;
+                        break;
+                    case 'L':
+                        lateCounter++;
+                        if (lateCounter >= 3)
+                            absenceCriteria = false;
+                        break;
+                    default:
+                        lateCounter = 0;
+                        break;
+                }
+
+            return absenceCriteria && lateCriteria;
+        }
+    }
+}

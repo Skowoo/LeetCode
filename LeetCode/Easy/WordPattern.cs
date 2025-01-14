67c0c0a5728d46f7084ac715b758b88370cc59ad
Skowoo <skowoo@tlen.pl>
2025-01-14 06:20:23 +0100

    WordPattern done

---
+++ LeetCode/Easy/WordPattern.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class WordPattern
+    {
+        public static bool WordPattern_Func(string pattern, string s)
+        {
+            Dictionary<char, string> charStringPairs = [];
+            Dictionary<string, char> stringCharPairs = [];
+            var words = s.Split(' ');
+
+            if (pattern.Length != words.Length)
+                return false;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                charStringPairs.TryAdd(pattern[i], words[i]);
+                stringCharPairs.TryAdd(words[i], pattern[i]);
+
+                if (charStringPairs[pattern[i]] != words[i])
+                    return false;
+                if (stringCharPairs[words[i]] != pattern[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}

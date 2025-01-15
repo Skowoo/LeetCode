7eed6f63efb7354fc905336845d19a3b10a805ee
Skowoo <skowoo@tlen.pl>
2025-01-15 06:02:53 +0100

    FirstBadVersion done

---
+++ LeetCode/Easy/FirstBadVersion.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class FirstBadVersion
+    {
+        public static int FirstBadVersion_Func(int n)
+        {
+            int max = n,
+                min = 1,
+                result = 0;
+
+            while (min <= max)
+            {
+                int mid = min + (max - min) / 2;
+                if (IsBadVersion(mid))
+                {
+                    result = mid;
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Mockup class
+        /// </summary>
+        private static bool IsBadVersion(int mid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

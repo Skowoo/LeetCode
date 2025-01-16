8602725896547c383e042a707d5b2be11b9f44b7
Skowoo <skowoo@tlen.pl>
2025-01-16 06:13:16 +0100

    DistributeCandies done

---
+++ LeetCode/Easy/DistributeCandies.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class DistributeCandies
+    {
+        public static int DistributeCandies_Func(int[] candyType)
+        {
+            var set = new HashSet<int>(candyType);
+            return Math.Min(set.Count, candyType.Length / 2);
+        }
+    }
+}

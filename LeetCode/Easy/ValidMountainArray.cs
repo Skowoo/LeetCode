c0245adfacd274551685e29fc8b06061e43a1560
Skowoo <skowoo@tlen.pl>
2025-01-17 06:15:10 +0100

    ValidMountainArray done

---
+++ LeetCode/Easy/ValidMountainArray.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ValidMountainArray
+    {
+        public static bool ValidMountainArray_Func(int[] arr)
+        {
+            int index = 0;
+
+            while (index + 1 < arr.Length && arr[index] < arr[index + 1])
+                index++;
+
+            if (index == 0 || index == arr.Length - 1)
+                return false;
+
+            while (index + 1 < arr.Length && arr[index] > arr[index + 1])
+                index++;
+
+            return index == arr.Length - 1;
+        }
+    }
+}

1160f2046710faf0c6a66e737be0d6e660153b7b
Skowoo <skowoo@tlen.pl>
2025-01-18 08:24:49 +0100

    HappyNumber done

---
+++ LeetCode/Easy/HappyNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class HappyNumber
+    {
+        public static bool IsHappy(int n)
+        {
+            if (n < 10)
+                return n == 1 || n == 7;
+
+            int tempSum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                n /= 10;
+                tempSum += digit * digit;
+            }
+
+            return IsHappy(tempSum);
+        }
+    }
+}

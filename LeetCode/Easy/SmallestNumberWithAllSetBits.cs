6f40b7d16aaca154eb8777621353ce88feb51bd1
Skowoo <skowoo@tlen.pl>
2025-01-21 06:08:42 +0100

    SmallestNumberWithAllSetBits done

---
+++ LeetCode/Easy/SmallestNumberWithAllSetBits.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SmallestNumberWithAllSetBits
+    {
+        public static int SmallestNumber(int n)
+        {
+            int result = 2;
+            while (result - 1 < n)
+                result *= 2;
+
+            return result - 1;
+        }
+    }
+}

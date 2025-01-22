bd3da31f3375dece63fcfc2508311439e1d1d686
Skowoo <skowoo@tlen.pl>
2025-01-22 06:01:54 +0100

    NeitherMinimumNorMaximum done

---
+++ LeetCode/Easy/NeitherMinimumNorMaximum.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class NeitherMinimumNorMaximum
+    {
+        public static int FindNonMinOrMax(int[] nums)
+        {
+            if (nums.Length < 3)
+                return -1;
+
+            Array.Sort(nums);
+            return nums[1];
+        }
+    }
+}

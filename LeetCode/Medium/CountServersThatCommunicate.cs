7879a34742528c2b39086992fb84730fd3ca58f0
Skowoo <skowoo@tlen.pl>
2025-01-23 06:24:56 +0100

    CountServersThatCommunicate done

---
+++ LeetCode/Medium/CountServersThatCommunicate.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class CountServersThatCommunicate
+    {
+        public static int CountServers(int[][] grid)
+        {
+            int result = 0;
+            int[] rowCounter = new int[grid.Length];
+            int[] colCounter = new int[grid[0].Length];
+
+            for (int i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[0].Length; j++)
+                    if (grid[i][j] == 1)
+                    {
+                        rowCounter[i]++;
+                        colCounter[j]++;
+                    }
+
+            for (int i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[0].Length; j++)
+                    if (grid[i][j] == 1 && (rowCounter[i] > 1 || colCounter[j] > 1))
+                        result++;
+
+            return result;
+        }
+    }
+}

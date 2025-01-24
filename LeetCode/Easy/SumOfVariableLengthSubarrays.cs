2c2fc9409cf3023c17e06fb711aae2d43474c71b
Skowoo <skowoo@tlen.pl>
2025-01-24 05:57:43 +0100

    SumOfVariableLengthSubarrays done

---
+++ LeetCode/Easy/SumOfVariableLengthSubarrays.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class SumOfVariableLengthSubarrays
+    {
+        public static int SubarraySum(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+                for (int start = Math.Max(0, i - nums[i]); start <= i; start++)
+                    result += nums[start];
+            return result;
+        }
+    }
+}

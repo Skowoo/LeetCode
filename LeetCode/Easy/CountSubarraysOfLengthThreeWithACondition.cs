8fb6a2e66808ca59d947f7634f5a29a3bfb1c8ff
Skowoo <skowoo@tlen.pl>
2025-01-31 06:04:58 +0100

    CountSubarraysOfLengthThreeWithACondition done

---
+++ LeetCode/Easy/CountSubarraysOfLengthThreeWithACondition.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CountSubarraysOfLengthThreeWithACondition
+    {
+        public static int CountSubarrays(int[] nums)
+        {
+            int result = 0;
+            for (int i = 1; i < nums.Length - 1; i++)
+                if ((nums[i - 1] + nums[i + 1]) * 2 == nums[i])
+                    result++;
+
+            return result;
+        }
+    }
+}

75974f4400f82c4c4403a07f014406640e9eac1f
Skowoo <skowoo@tlen.pl>
2025-02-01 08:26:32 +0100

    SpecialArrayI done

---
+++ LeetCode/Easy/SpecialArrayI.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal class SpecialArrayI
+    {
+        public static bool IsArraySpecial(int[] nums)
+        {
+            if (nums.Length < 2)
+                return true;
+
+            for (int i = 1; i < nums.Length; i++)
+                if (nums[i] % 2 == nums[i - 1] % 2)
+                    return false;
+
+            return true;
+        }
+    }
+}

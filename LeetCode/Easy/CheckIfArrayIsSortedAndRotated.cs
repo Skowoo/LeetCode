cf8aee6ab8c2eeb06ba5eaefe5fcb66685dbfa74
Skowoo <skowoo@tlen.pl>
2025-02-02 07:57:45 +0100

    CheckIfArrayIsSortedAndRotated done

---
+++ LeetCode/Easy/CheckIfArrayIsSortedAndRotated.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CheckIfArrayIsSortedAndRotated
+    {
+        public static bool Check(int[] nums)
+        {
+            bool wasRotated = false;
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] > nums[(i + 1) % nums.Length] && !wasRotated)
+                    wasRotated = true;
+                else if (nums[i] > nums[(i + 1) % nums.Length])
+                    return false;
+
+            return true;
+        }
+    }
+}

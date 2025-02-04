8ca534b4ebb48bfd887433146669df6c9d75ac6a
Skowoo <skowoo@tlen.pl>
2025-02-04 08:32:07 +0100

    MaximumAscendingSubarraySum done

---
+++ LeetCode/Easy/MaximumAscendingSubarraySum.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class MaximumAscendingSubarraySum
+    {
+        public static int MaxAscendingSum(int[] nums)
+        {
+            int maxSum = 0;
+            int currSum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                    currSum += nums[i];
+                else
+                {
+                    maxSum = Math.Max(maxSum, currSum);
+                    currSum = nums[i];
+                }
+            }
+
+            return Math.Max(maxSum, currSum);
+        }
+    }
+}

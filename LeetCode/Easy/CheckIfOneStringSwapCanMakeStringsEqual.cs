35bd18fe1903f8a9e823de3b38b0760f4afcfc34
Skowoo <skowoo@tlen.pl>
2025-02-05 06:16:38 +0100

    CheckIfOneStringSwapCanMakeStringsEqual done

---
+++ LeetCode/Easy/CheckIfOneStringSwapCanMakeStringsEqual.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CheckIfOneStringSwapCanMakeStringsEqual
+    {
+        public static bool AreAlmostEqual(string s1, string s2)
+        {
+            string? discrepancy = null;
+            bool haveHappened = false;
+            for (int i = 0; i < s1.Length; i++)
+                if (s1[i] != s2[i])
+                    if (discrepancy is null)
+                        discrepancy = s1[i].ToString() + s2[i].ToString();
+                    else if (discrepancy == s2[i].ToString() + s1[i].ToString() && !haveHappened)
+                        haveHappened = true;
+                    else
+                        return false;
+
+            return discrepancy is null || haveHappened && discrepancy is not null;
+        }
+    }
+}

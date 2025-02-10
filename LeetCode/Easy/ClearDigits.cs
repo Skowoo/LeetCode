61d79fa2cf8d1e22a76d3107031858a68889b597
Skowoo <skowoo@tlen.pl>
2025-02-10 06:20:22 +0100

    ClearDigits done

---
+++ LeetCode/Easy/ClearDigits.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ClearDigitsClass
+    {
+        public static string ClearDigits(string s)
+        {
+            if (s.Length < 2)
+                return s;
+
+            for (int i = 1; i < s.Length; i++)
+                if (!char.IsLetter(s[i]))
+                {
+                    s = s.Remove(i - 1, 2);
+                    return ClearDigits(s);
+                }
+
+            return s;
+        }
+    }
+}

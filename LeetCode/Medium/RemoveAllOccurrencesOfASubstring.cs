62c2dbd272947ee3ee783187e2ad89b429aaaf83
Skowoo <skowoo@tlen.pl>
2025-02-11 06:13:20 +0100

    RemoveAllOccurrencesOfASubstring done

---
+++ LeetCode/Medium/RemoveAllOccurrencesOfASubstring.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class RemoveAllOccurrencesOfASubstring
+    {
+        public static string RemoveOccurrences(string s, string part)
+        {
+            int index;
+            if ((index = s.IndexOf(part)) != -1)
+                return RemoveOccurrences(s.Remove(index, part.Length), part);
+            else
+                return s;
+        }
+    }
+}

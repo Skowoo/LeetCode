a97629c9a5c112add4e7de2ce5245af4519cb796
Skowoo <skowoo@tlen.pl>
2025-02-14 06:09:57 +0100

    MaximumNumberOfBalloons done

---
+++ LeetCode/Easy/MaximumNumberOfBalloons.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class MaximumNumberOfBalloons
+    {
+        public static int MaxNumberOfBalloons(string text)
+        {
+            Dictionary<char, int> letters = new()
+            {
+                { 'b', 0 },
+                { 'a', 0 },
+                { 'l', 0 },
+                { 'o', 0 },
+                { 'n', 0 }
+            };
+
+            foreach (char letter in text)
+                if (letters.TryGetValue(letter, out int value))
+                    letters[letter] = ++value;
+
+            letters['o'] /= 2;
+            letters['l'] /= 2;
+
+            int result = int.MaxValue;
+            foreach (var letter in letters)
+                result = Math.Min(result, letter.Value);
+
+            return result;
+        }
+    }
+}

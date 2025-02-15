f3936c3c7b03fb69580cc5843d931e0d710d98f1
Skowoo <skowoo@tlen.pl>
2025-02-15 07:36:41 +0100

    ReplaceAllDigitsWithCharacters done

---
+++ LeetCode/Easy/ReplaceAllDigitsWithCharacters.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal static class ReplaceAllDigitsWithCharacters
+    {
+        public static string ReplaceDigits(string s)
+        {
+            StringBuilder sb = new(s);
+
+            for (int i = 1; i < s.Length; i += 2)
+                sb[i] = (char)(s[i - 1] + (s[i] - 48));
+
+            return sb.ToString();
+        }
+    }
+}

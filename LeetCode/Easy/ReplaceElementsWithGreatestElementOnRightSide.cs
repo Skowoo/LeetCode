129fa4187e5a3dc7087a31b46ff4eb6a874962b3
Skowoo <skowoo@tlen.pl>
2025-02-16 08:16:05 +0100

    ReplaceElementsWithGreatestElementOnRightSide done

---
+++ LeetCode/Easy/ReplaceElementsWithGreatestElementOnRightSide.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ReplaceElementsWithGreatestElementOnRightSide
+    {
+        public static int[] ReplaceElements(int[] arr)
+        {
+            int prevMax = -1;
+            int currNum;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                currNum = arr[i];
+                arr[i] = prevMax;
+                prevMax = Math.Max(prevMax, currNum);
+            }
+
+            return arr;
+        }
+    }
+}

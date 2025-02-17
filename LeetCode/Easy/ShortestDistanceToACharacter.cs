3e424731915f5d4b326a1699a71786c9b7fe9d36
Skowoo <skowoo@tlen.pl>
2025-02-17 08:46:07 +0100

    ShortestDistanceToACharacter done

---
+++ LeetCode/Easy/ShortestDistanceToACharacter.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ShortestDistanceToACharacter
+    {
+        public static int[] ShortestToChar(string s, char c)
+        {
+            var result = new int[s.Length];
+
+            int distance = s.Length;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c)
+                    distance = 0;
+
+                result[i] = distance;
+
+                distance++;
+            }
+
+            distance = s.Length;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == c)
+                    distance = 0;
+
+                result[i] = Math.Min(distance, result[i]);
+
+                distance++;
+            }
+
+            return result;
+        }
+    }
+}

b232e0054acc3e31b09855027f8528afddaa7ee1
Skowoo <skowoo@tlen.pl>
2025-02-18 08:15:09 +0100

    ReformatDate done

---
+++ LeetCode/Easy/ReformatDate.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ReformatDate
+    {
+        public static string ReformatDate_Func(string date)
+        {
+            var parts = date.Split(' ');
+            string[] months = ["Zero", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            return $"{parts[2]}-{Array.IndexOf(months, parts[1]):D2}-{int.Parse(parts[0][..^2]):D2}";
+        }
+    }
+}

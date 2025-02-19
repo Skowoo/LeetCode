a57bdf71bed6783c274c32364525b05a83af2d9f
Skowoo <skowoo@tlen.pl>
2025-02-19 06:23:51 +0100

    CreateTargetArrayInTheGivenOrder done

---
+++ LeetCode/Easy/CreateTargetArrayInTheGivenOrder.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CreateTargetArrayInTheGivenOrder
+    {
+        public static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            var result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = nums.Length - 1; j > index[i]; j--)
+                    result[j] = result[j - 1];
+
+                result[index[i]] = nums[i];
+            }
+
+            return result;
+        }
+    }
+}

4c1194070e1eeeed87618c3c5bbea748f4725d2c
Skowoo <skowoo@tlen.pl>
2025-02-23 08:23:44 +0100

    SubtractTheProductAndSumOfDigitsOfAnInteger done

---
+++ LeetCode/Easy/SubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    public static class SubtractTheProductAndSumOfDigitsOfAnInteger
+    {
+        public static int SubtractProductAndSum(int n)
+        {
+            int product = 1,
+                sum = 0;
+
+            foreach (var d in n.ToString())
+            {
+                int digit = int.Parse(d.ToString());
+                product *= digit;
+                sum += digit;
+            }
+
+            return product - sum;
+        }
+    }
+}

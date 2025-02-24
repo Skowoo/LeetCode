e95786a6c01f9c200c7d152a6d9e52190017a08a
Skowoo <skowoo@tlen.pl>
2025-02-24 06:20:34 +0100

    ShuffleTheArray done

---
+++ LeetCode/Easy/ShuffleTheArray.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ShuffleTheArray
+    {
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[nums.Length];
+            int firstPointer = 0,
+                secondPointer = n;
+
+            for (int i = 0; i < result.Length; i++)
+                if (i % 2 == 0)
+                    result[i] = nums[firstPointer++];
+                else
+                    result[i] = nums[secondPointer++];
+
+            return result;
+        }
+    }
+}

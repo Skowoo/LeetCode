d2e2a5531886f777655fdcddd31f9778366b42a7
Skowoo <skowoo@tlen.pl>
2025-02-25 18:16:15 +0100

    ConvertDateToBinary done

---
+++ LeetCode/Easy/ConvertDateToBinary.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class ConvertDateToBinary
+    {
+        public static string ConvertDateToBinary_Func(string date)
+        {
+            var parts = date.Split('-');
+            List<string> result = [];
+            foreach (var part in parts)
+                result.Add(Convert.ToString(int.Parse(part), 2));
+
+            return String.Join('-', result);
+        }
+    }
+}

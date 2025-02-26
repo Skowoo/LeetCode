c649f5611031c3a6c650a4bee992a730851f07aa
Skowoo <skowoo@tlen.pl>
2025-02-26 06:14:12 +0100

    MinimumNumberGame done

---
+++ LeetCode/Easy/MinimumNumberGame.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class MinimumNumberGame
+    {
+        public static int[] NumberGame(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 1; i < nums.Length; i += 2)
+                (nums[i - 1], nums[i]) = (nums[i], nums[i - 1]);
+            return nums;
+        }
+    }
+}

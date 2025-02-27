c5a6dcf78e85b55092676a0170189158a8cad923
Skowoo <skowoo@tlen.pl>
2025-02-27 06:17:18 +0100

    NumberOfEvenAndOddBits done

---
+++ LeetCode/Easy/NumberOfEvenAndOddBits.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class NumberOfEvenAndOddBits
+    {
+        public static int[] EvenOddBit(int n)
+        {
+            string binaryReversed = new([.. Convert.ToString(n, 2).Reverse()]);
+            int odd = 0,
+                even = 0;
+
+            for (int i = 0; i < binaryReversed.Length; i++)
+                if (i % 2 == 0 && binaryReversed[i] == '1')
+                    even++;
+                else if (binaryReversed[i] == '1')
+                    odd++;
+
+            return [even, odd];
+        }
+    }
+}

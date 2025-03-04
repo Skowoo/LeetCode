46f058247078a387996265c812075d62cc3a6405
Skowoo <skowoo@tlen.pl>
2025-03-04 06:16:45 +0100

    CheckIfNumberIsASumOfPowersOfThree done

---
+++ LeetCode/Medium/CheckIfNumberIsASumOfPowersOfThree.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class CheckIfNumberIsASumOfPowersOfThree
+    {
+        public static bool CheckPowersOfThree(int n)
+        {
+            while (n > 1)
+            {
+                int rest = n % 3;
+                if (rest == 2)
+                    return false;
+                else
+                    n /= 3;
+            }
+            return true;
+        }
+    }
+}

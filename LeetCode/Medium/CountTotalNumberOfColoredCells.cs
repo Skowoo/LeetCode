6d380b92f622a3fca0b15d342fc7d1df2df0dd8f
Skowoo <skowoo@tlen.pl>
2025-03-05 13:40:18 +0100

    CountTotalNumberOfColoredCells done

---
+++ LeetCode/Medium/CountTotalNumberOfColoredCells.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class CountTotalNumberOfColoredCells
+    {
+        public static long ColoredCells(int n)
+        {
+            return 2 * (long)Math.Pow(n, 2) - 2 * n + 1;
+        }
+    }
+}

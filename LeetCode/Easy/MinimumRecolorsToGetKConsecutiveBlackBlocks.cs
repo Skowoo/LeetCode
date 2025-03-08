9646c81f713c6020fc77e953a60c91eab5059d04
Skowoo <skowoo@tlen.pl>
2025-03-08 08:20:05 +0100

    MinimumRecolorsToGetKConsecutiveBlackBlocks done

---
+++ LeetCode/Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class MinimumRecolorsToGetKConsecutiveBlackBlocks
+    {
+        public static int MinimumRecolors(string blocks, int k)
+        {
+            int whiteCount = 0;
+
+            for (int i = 0; i < k; i++)
+                if (blocks[i] == 'W')
+                    whiteCount++;
+
+            int result = whiteCount;
+
+            for (int i = k; i < blocks.Length; i++)
+            {
+                if (blocks[i] == 'W')
+                    whiteCount++;
+
+                if (blocks[i - k] == 'W')
+                    whiteCount--;
+
+                result = Math.Min(result, whiteCount);
+            }
+
+            return result;
+        }
+    }
+}

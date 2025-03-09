c0eb4403e64c63c4d3dd9ad3016a3769b47120cc
Skowoo <skowoo@tlen.pl>
2025-03-09 07:55:00 +0100

    AlternatingGroupsII done

---
+++ LeetCode/Medium/AlternatingGroupsII.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class AlternatingGroupsII
+    {
+        public static int NumberOfAlternatingGroups(int[] colors, int k)
+        {
+            int left = 0,
+                result = 0;
+
+            for (int right = 0; right < colors.Length + k - 1; right++)
+            {
+                if (colors[right % colors.Length] == colors[(right + 1) % colors.Length])
+                    left = right;
+
+                if (right - left + 1 == k)
+                {
+                    result++;
+                    left++;
+                }
+            }
+
+            return result;
+        }
+    }
+}

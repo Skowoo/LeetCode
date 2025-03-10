d70fc7cd60e99f6bea10e7a7447ad014e21af209
Skowoo <skowoo@tlen.pl>
2025-03-10 06:23:11 +0100

    DetermineColorOfAChessboardSquare done

---
+++ LeetCode/Easy/DetermineColorOfAChessboardSquare.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class DetermineColorOfAChessboardSquare
+    {
+        public static bool SquareIsWhite(string coordinates)
+        {
+            if (coordinates[0] % 2 == 1)
+            {
+                if (coordinates[1] % 2 == 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (coordinates[1] % 2 == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}

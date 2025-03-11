df8e0733aa86fca6dbd9c5b55fee938ee08b41c8
Skowoo <skowoo@tlen.pl>
2025-03-11 14:21:42 +0100

    NumberOfSubstringsContainingAllThreeCharacters done

---
+++ LeetCode/Medium/NumberOfSubstringsContainingAllThreeCharacters.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Medium
+{
+    internal static class NumberOfSubstringsContainingAllThreeCharacters
+    {
+        public static int NumberOfSubstrings(string s)
+        {
+            int result = 0;
+            int left = 0;
+            Dictionary<char, int> dict = [];
+            dict.Add('a', 0);
+            dict.Add('b', 0);
+            dict.Add('c', 0);
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                dict[s[right]]++;
+
+                while (dict['a'] > 0 && dict['b'] > 0 && dict['c'] > 0)
+                {
+                    result += s.Length - right;
+                    dict[s[left++]]--;
+                }
+            }
+
+            return result;
+        }
+    }
+}

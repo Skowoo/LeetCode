78febb7867e63e4f6a25ddabe5d45806aa266b52
Skowoo <skowoo@tlen.pl>
2025-03-12 06:11:41 +0100

    MaximumCountOfPositiveIntegerAndNegativeInteger done

---
+++ LeetCode/Easy/MaximumCountOfPositiveIntegerAndNegativeInteger.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class MaximumCountOfPositiveIntegerAndNegativeInteger
+    {
+        public static int MaximumCount(int[] nums)
+        {
+            int positiveCount = 0,
+                negativeCount = 0;
+
+            foreach (var num in nums)
+                if (num > 0)
+                    positiveCount++;
+                else if (num < 0)
+                    negativeCount++;
+
+            return Math.Max(positiveCount, negativeCount);
+        }
+    }
+}

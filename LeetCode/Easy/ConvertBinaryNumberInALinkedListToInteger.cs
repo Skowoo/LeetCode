06c32d5413c7888fda665c838c3a1f357384a2d2
Skowoo <skowoo@tlen.pl>
2025-03-14 07:59:36 +0100

    ConvertBinaryNumberInALinkedListToInteger done

---
+++ LeetCode/Easy/ConvertBinaryNumberInALinkedListToInteger.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.CommonClasses;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal static class ConvertBinaryNumberInALinkedListToInteger
+    {
+        public static int GetDecimalValue_String(ListNode head)
+        {
+            StringBuilder sb = new();
+            while (head is not null)
+            {
+                sb.Append(head.val);
+                head = head.next;
+            }
+            return Convert.ToInt32(sb.ToString(), 2);
+        }
+    }
+}

36e7327b91cdfab155d0fbd89e61e5c0d6d487ea
Skowoo <skowoo@tlen.pl>
2025-03-20 10:54:34 +0100

    ExcelSheetColumnTitle done

---
+++ LeetCode/Easy/ExcelSheetColumnTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LeetCode.Easy
+{
+    internal static class ExcelSheetColumnTitle
+    {
+        public static string ConvertToTitle(int columnNumber)
+        {
+            StringBuilder sb = new();
+
+            while (columnNumber > 0)
+            {
+                char letter = (char)(((columnNumber - 1) % 26) + 65);
+                sb.Insert(0, letter);
+                columnNumber = (columnNumber - 1) / 26;
+            }
+
+            return sb.ToString();
+        }
+    }
+}

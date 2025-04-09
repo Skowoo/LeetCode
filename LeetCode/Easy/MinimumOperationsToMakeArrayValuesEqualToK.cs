236dd2638a2c425f1e71f3a86a6937b9e7442994
Skowoo <skowoo@tlen.pl>
2025-04-09 08:48:16 +0200

    MinimumOperationsToMakeArrayValuesEqualToK done

---
+++ LeetCode/Easy/MinimumOperationsToMakeArrayValuesEqualToK.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class MinimumOperationsToMakeArrayValuesEqualToK
+    {
+        public static int MinOperations(int[] nums, int k)
+        {
+            HashSet<int> hashSet = [];
+            foreach (int num in nums)
+                if (num < k)
+                    return -1;
+                else if (num > k)
+                    hashSet.Add(num);
+
+            return hashSet.Count;
+        }
+    }
+}

b3354cfeba0d9b9b9e2ddd0829dd3e18726c63be
Skowoo <skowoo@tlen.pl>
2025-04-10 13:00:29 +0200

    CapitalizeTheTitle done

---
+++ LeetCode/Easy/CapitalizeTheTitle.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Easy
+{
+    internal static class CapitalizeTheTitle
+    {
+        public static string CapitalizeTitle(string title)
+        {
+            string[] words = title.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                if (words[i].Length > 2)
+                    words[i] = words[i][..1].ToUpper() + words[i][1..].ToLower();
+                else
+                    words[i] = words[i].ToLower();
+
+            return string.Join(" ", words);
+        }
+    }
+}

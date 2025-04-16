f0585b1f7d1f033ee1fc523c11de1ab0093722d8
Skowoo <skowoo@tlen.pl>
2025-04-16 09:28:41 +0200

    InsertGreatestCommonDivisorsInLinkedList done

---
+++ LeetCode/Medium/InsertGreatestCommonDivisorsInLinkedList.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.CommonClasses;
+using System.Numerics;
+
+namespace LeetCode.Medium
+{
+    internal static class InsertGreatestCommonDivisorsInLinkedList
+    {
+        public static ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            ListNode currentNode = head;
+            ListNode nextNode;
+
+            while (currentNode.next is not null)
+            {
+                nextNode = currentNode.next;
+                int newNodeValue = (int)BigInteger.GreatestCommonDivisor(currentNode.val, nextNode.val);
+                currentNode.next = new ListNode(newNodeValue, nextNode);
+                currentNode = nextNode;
+            }
+
+            return head;
+        }
+    }
+}
